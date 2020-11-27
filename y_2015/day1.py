4019ba5e88d13bd4f5b8a5417fa6f8825382f8b2
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-27 09:44:44 -0800

    refactor calculate1

--- y_2015/day1.py
+++ y_2015/day1.py
@@ -1,10 +1,11 @@
+OPERATIONS = {"(": 1, ")": -1}
+
+
 def calculate_1(i: str):
     result = 0
     for x in i:
-        if x == "(":
-            result += 1
-        elif x == ")":
-            result -= 1
+        result += OPERATIONS[x]
+
     return result
 
 
