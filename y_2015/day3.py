65a0fb67f82b683865b45a74db3f7477792db32f
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-27 12:57:12 -0800

    remove prints

--- y_2015/day3.py
+++ y_2015/day3.py
@@ -9,16 +9,12 @@ OPERATIONS = {">": (1, 0), "<": (-1, 0), "^": (0, 1), "v": (0, -1)}
 
 
 def calculate_1(i: str) -> int:
-    # print(i)
     grid = defaultdict(int)
     x = 1
     y = 1
     grid[(x, y)] = 1
     for j in i:
-        # print(x)
         moves = OPERATIONS[j]
-        # print(moves)
-        # breakpoint()
         x += moves[0]
         y += moves[1]
         grid[(x, y)] = 1
@@ -26,7 +22,6 @@ def calculate_1(i: str) -> int:
 
 
 def calculate_2(i: str) -> int:
-    # print(i)
     grid = defaultdict(int)
     sx = 1
     sy = 1
@@ -35,10 +30,7 @@ def calculate_2(i: str) -> int:
     grid[(1, 1)] = 1
     s_moves = 1
     for j in i:
-        # print(x)
         moves = OPERATIONS[j]
-        # print(moves)
-        # breakpoint()
         if s_moves:
             sx += moves[0]
             sy += moves[1]
