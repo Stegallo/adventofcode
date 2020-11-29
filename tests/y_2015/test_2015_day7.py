677014eaee548421271b9681a607711eaed6cbd1
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-28 18:59:28 -0800

    day 7 (no idea why it works)

---
+++ tests/y_2015/test_2015_day7.py
@@ -0,0 +1,85 @@
+from unittest.mock import patch
+
+from y_2015.day7 import calculate_1, calculate_2, inner_1, parse
+
+
+def test_parse():
+    assert parse("123 -> x") == ["x", "123"]
+    assert parse("x AND y -> z") == ["z", "AND", "x", "y"]
+    assert parse("p LSHIFT 2 -> q") == ["q", "LSHIFT", "p", "2"]
+    assert parse("NOT e -> f") == ["f", "NOT", "e"]
+    assert parse("x OR y -> z") == ["z", "OR", "x", "y"]
+    assert parse("p RSHIFT 2 -> q") == ["q", "RSHIFT", "p", "2"]
+
+
+def test_inner_1():
+    assert inner_1(
+        [
+            "123 -> x",
+            "456 -> y",
+            "x AND y -> d",
+            "x OR y -> e",
+            "x LSHIFT 2 -> f",
+            "y RSHIFT 2 -> g",
+            "NOT x -> h",
+            "NOT y -> i",
+        ]
+    ) == {
+        "d": 72,
+        "e": 507,
+        "f": 492,
+        "g": 114,
+        "h": 65412,
+        "i": 65079,
+        "x": 123,
+        "y": 456,
+    }
+
+
+def test_inner_1_bis():
+    assert inner_1(
+        [
+            "123 -> x",
+            "456 -> y",
+            "x AND y -> d",
+            "x OR y -> e",
+            "x LSHIFT 2 -> f",
+            "y RSHIFT 2 -> g",
+            "NOT x -> h",
+            "NOT y -> i",
+            "x AND 456 -> j",
+        ]
+    ) == {
+        "d": 72,
+        "e": 507,
+        "f": 492,
+        "g": 114,
+        "h": 65412,
+        "i": 65079,
+        "x": 123,
+        "y": 456,
+        "j": 72,
+    }
+
+
+def test_calculate_1():
+    assert (
+        calculate_1(
+            [
+                "123 -> x",
+                "456 -> y",
+                "x AND y -> d",
+                "x OR y -> e",
+                "x LSHIFT 2 -> f",
+                "y RSHIFT 2 -> g",
+                "NOT x -> h",
+                "NOT y -> i",
+                "1 -> a",
+            ]
+        )
+        == 1
+    )
+
+
+def test_calculate_2():
+    assert True
---
+++ y_2015/day7.py
@@ -0,0 +1,198 @@
+from collections import defaultdict
+from typing import DefaultDict
+import re
+
+
+def parse(i):
+    x = i[: re.search(" ->", i).start()]
+    if "AND" in x:
+        # print(re.search(" AND", x).start())
+        first = x[: re.search(" AND", x).start()]
+        second = x[re.search(" AND", x).start() + 5 :]
+
+        return [i[re.search(" ->", i).start() + 4 :], "AND", first, second]
+
+    if "OR" in x:
+        # print(re.search(" OR", x).start())
+        first = x[: re.search(" OR", x).start()]
+        second = x[re.search(" OR", x).start() + 4 :]
+
+        return [i[re.search(" ->", i).start() + 4 :], "OR", first, second]
+
+    if "LSHIFT" in x:
+        # print(re.search(" LSHIFT", x).start())
+        first = x[: re.search(" LSHIFT", x).start()]
+        second = x[re.search(" LSHIFT", x).start() + 8 :]
+
+        return [i[re.search(" ->", i).start() + 4 :], "LSHIFT", first, second]
+
+    if "RSHIFT" in x:
+        # print(re.search(" RSHIFT", x).start())
+        first = x[: re.search(" RSHIFT", x).start()]
+        second = x[re.search(" RSHIFT", x).start() + 8 :]
+
+        return [i[re.search(" ->", i).start() + 4 :], "RSHIFT", first, second]
+
+    if "NOT" in x:
+        # print(re.search("NOT", x).start())
+        return [
+            i[re.search(" ->", i).start() + 4 :],
+            "NOT",
+            x[re.search("NOT", x).start() + 4 :],
+        ]
+
+    return [i[re.search(" ->", i).start() + 4 :], i[: re.search(" ->", i).start()]]
+
+
+def evaluate(i):
+
+    solved = False
+    while not solved:
+        no_evaluated = True
+        for j in i:
+            if len(i[j]) > 1:
+                no_evaluated = False
+
+                if i[j][0] == "AND":
+                    # breakpoint()
+                    try:
+                        i[j] = [int(i[i[j][1]][0]) & int(i[i[j][2]][0])]
+                    except ValueError:
+                        pass
+                    except KeyError as e:
+                        first = None
+                        second = None
+                        try:
+                            first = int(i[j][1])
+                        except ValueError:
+                            pass
+                        try:
+                            second = int(i[j][2])
+                        except ValueError:
+                            pass
+                        try:
+                            i[j] = [
+                                (first or int(i[i[j][1]][0]))
+                                & (second or int(i[i[j][2]][0]))
+                            ]
+                        except ValueError:
+                            pass
+
+                if i[j][0] == "OR":
+                    try:
+                        i[j] = [int(i[i[j][1]][0]) | int(i[i[j][2]][0])]
+                    except ValueError:
+                        pass
+                    except KeyError as e:
+                        first = None
+                        second = None
+                        try:
+                            first = int(i[j][1])
+                        except ValueError:
+                            pass
+                        try:
+                            second = int(i[j][2])
+                        except ValueError:
+                            pass
+                        try:
+                            i[j] = [
+                                (first or int(i[i[j][1]][0]))
+                                | (second or int(i[i[j][2]][0]))
+                            ]
+                        except ValueError:
+                            pass
+
+                if i[j][0] == "LSHIFT":
+                    try:
+                        i[j] = [int(i[i[j][1]][0]) << int(i[j][2])]
+                    except ValueError:
+                        pass
+                    except KeyError as e:
+                        first = None
+                        second = None
+                        try:
+                            first = int(i[j][1])
+                        except ValueError:
+                            pass
+                        try:
+                            second = int(i[j][2])
+                        except ValueError:
+                            pass
+                        try:
+                            i[j] = [
+                                (first or int(i[i[j][1]][0]))
+                                << (second or int(i[i[j][2]][0]))
+                            ]
+                        except ValueError:
+                            pass
+
+                if i[j][0] == "RSHIFT":
+                    try:
+                        i[j] = [int(i[i[j][1]][0]) >> int(i[j][2])]
+                    except ValueError:
+                        pass
+                    except KeyError as e:
+                        first = None
+                        second = None
+                        try:
+                            first = int(i[j][1])
+                        except ValueError:
+                            pass
+                        try:
+                            second = int(i[j][2])
+                        except ValueError:
+                            pass
+                        try:
+                            i[j] = [
+                                (first or int(i[i[j][1]][0]))
+                                >> (second or int(i[i[j][2]][0]))
+                            ]
+                        except ValueError:
+                            pass
+
+                if i[j][0] == "NOT":
+                    try:
+                        i[j] = [int(i[i[j][1]][0]) ^ 65535]
+                    except ValueError:
+                        pass
+                    except KeyError as e:
+                        first = None
+                        try:
+                            first = int(i[j][1])
+                        except ValueError:
+                            pass
+                        try:
+                            i[j] = [(first or int(i[i[j][1]][0])) ^ 65535]
+                        except ValueError:
+                            pass
+
+            else:
+                try:
+                    i[j] = [int(i[j][0])]
+                except ValueError:
+                    pass
+        if no_evaluated:
+            solved = True
+    print(i)
+    return {k: int(v[0]) for k, v in i.items()}
+
+
+def inner_1(lista):
+    result = {}
+    parsedl = [parse(i) for i in lista]
+    for i in parsedl:
+        # print(i)
+        result[i[0]] = i[1:]
+    result = evaluate(result)
+    return result
+
+
+def calculate_1(x: list) -> int:
+    result = inner_1(x)
+    # print(result)
+    return result["a"]
+
+
+def calculate_2(x: str) -> int:
+
+    return 0
