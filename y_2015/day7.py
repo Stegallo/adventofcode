d850c736ce00f4d2b0a21d320b49cace030aad0d
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-29 12:34:14 -0800

    cleanup

--- y_2015/day7.py
+++ y_2015/day7.py
@@ -34,48 +34,36 @@ class Link:
     """docstring for Link"""
 
     @staticmethod
+    def get_link_from_value(x):
+        try:
+            value = int(x)
+            return ValueLink(value)
+        except ValueError:
+            pass
+        return DirectLink(WIRING[x])
+
+    @staticmethod
     def create_link(rule):
         if len(rule) == 1:
-            try:
-                value = int(rule[0])
-                return ValueLink(value)
-            except ValueError:
-                pass
-            return DirectLink(WIRING[rule[0]])
+            return Link.get_link_from_value(rule[0])
 
         if rule[0] in BIN_OPERATORS:
-            try:
-                value = int(rule[1])
-                op1 = ValueLink(value)
-            except ValueError:
-                op1 = DirectLink(WIRING[rule[1]])
-            try:
-                value = int(rule[2])
-                op2 = ValueLink(value)
-            except ValueError:
-                op2 = DirectLink(WIRING[rule[2]])
-
-            return BinaryLink(rule[0], op1, op2)
+            return BinaryLink(
+                rule[0],
+                Link.get_link_from_value(rule[1]),
+                Link.get_link_from_value(rule[2]),
+            )
 
         if rule[0] in UN_OPERATORS:
-            try:
-                value = int(rule[1])
-                op1 = ValueLink(value)
-            except ValueError:
-                op1 = DirectLink(WIRING[rule[1]])
-            return UnaryLink(rule[0], op1)
+            return UnaryLink(rule[0], Link.get_link_from_value(rule[1]))
 
-
-class DirectLink(Link):
-    """"""
-
-    def __init__(self, op1):
+    def __init__(self, operator=None):
         self._cached_result = None
-        self._op1 = op1
+        self._operator = operator
 
     def get_value(self):
         if not self._cached_result:
-            self._cached_result = self._op1.content.get_value()
+            self._cached_result = self._get_value()
         return self._cached_result
 
 
@@ -83,39 +71,48 @@ class ValueLink(Link):
     """"""
 
     def __init__(self, value):
-        self._value = value
+        self.__value = value
+        super().__init__()
 
-    def get_value(self):
-        return self._value
+    def _get_value(self):
+        return self.__value
+
+    def set_value(self, value):
+        self.__value = value
+
+
+class DirectLink(Link):
+    """"""
+
+    def __init__(self, op1):
+        self._op1 = op1
+        super().__init__()
+
+    def _get_value(self):
+        return self._op1.content.get_value()
 
 
 class BinaryLink(Link):
     """"""
 
     def __init__(self, operator, op1, op2):
-        self._cached_result = None
-        self._operator = operator
         self._op1 = op1
         self._op2 = op2
+        super().__init__(operator)
 
-    def get_value(self):
-        if not self._cached_result:
-            self._cached_result = BIN_OPERATORS[self._operator](
-                self._op1.get_value(), self._op2.get_value()
-            )
-        return self._cached_result
+    def _get_value(self):
+        return BIN_OPERATORS[self._operator](
+            self._op1.get_value(), self._op2.get_value()
+        )
 
 
 class UnaryLink(Link):
     def __init__(self, operator, op1):
-        self._cached_result = None
-        self._operator = operator
         self._op1 = op1
+        super().__init__(operator)
 
-    def get_value(self):
-        if not self._cached_result:
-            self._cached_result = UN_OPERATORS[self._operator](self._op1.get_value())
-        return self._cached_result
+    def _get_value(self):
+        return UN_OPERATORS[self._operator](self._op1.get_value())
 
 
 WIRING = defaultdict(Container)
@@ -180,7 +177,7 @@ def inner_2(lista):
     for i in parsedl:
         temp = WIRING[i[0]]
         temp.content = Link.create_link(i[1:])
-    WIRING["b"].content._value = 46065
+    WIRING["b"].content.set_value(46065)
     return {k: v.content.get_value() for k, v in WIRING.items()}
 
 
