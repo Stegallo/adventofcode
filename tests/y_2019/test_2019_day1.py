3930ae6cf724a2f3a4e9e96186910389014aafb0
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-29 14:15:28 -0800

    restructure 2019

--- tests/y_2019/test_2019_day1.py
+++ tests/y_2019/test_2019_day1.py
@@ -1,14 +1,22 @@
-from y_2019.day1 import calculate_1, calculate_2
+from y_2019.day1 import inner_1, inner_2, calculate_1, calculate_2
+
+
+def test_inner_1():
+    assert inner_1(12) == 2
+    assert inner_1(14) == 2
+    assert inner_1(1969) == 654
+    assert inner_1(100756) == 33583
 
 
 def test_calculate_1():
-    assert calculate_1(12) == 2
-    assert calculate_1(14) == 2
-    assert calculate_1(1969) == 654
-    assert calculate_1(100756) == 33583
+    assert calculate_1(["12"]) == 2
+
+
+def test_inner_2():
+    assert inner_2(14) == 2
+    assert inner_2(1969) == 966
+    assert inner_2(100756) == 50346
 
 
 def test_calculate_2():
-    assert calculate_2(14) == 2
-    assert calculate_2(1969) == 966
-    assert calculate_2(100756) == 50346
+    assert calculate_1(["14"]) == 2
