f4992c303c84c12a7ee3bb845e54dd4c0649dae8
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-30 21:23:15 -0800

    day 1

--- tests/y_2020/test_2020_day1.py
+++ tests/y_2020/test_2020_day1.py
@@ -2,8 +2,8 @@ from y_2020.day1 import calculate_1, calculate_2
 
 
 def test_calculate_1():
-    assert calculate_1(0) == 0
+    assert calculate_1([1721, 979, 366, 299, 675, 1456]) == 514579
 
 
 def test_calculate_2():
-    assert calculate_2(0) == 0
+    assert calculate_2([1721, 979, 366, 299, 675, 1456]) == 241861950
--- y_2020/day1.py
+++ y_2020/day1.py
@@ -1,6 +1,21 @@
 def calculate_1(x):
+    y = [int(i) for i in x]
+
+    for i in y:
+        for j in y:
+            if (i + j) == 2020:
+                return i * j
+
     return 0
 
 
 def calculate_2(x):
+    y = [int(i) for i in x]
+
+    for i in y:
+        for j in y:
+            for k in y:
+                if (i + j + k) == 2020:
+                    return i * j * k
+
     return 0
