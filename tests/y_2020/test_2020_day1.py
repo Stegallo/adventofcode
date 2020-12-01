1298fbfe80a13984dae87f444c9977a997129eb5
Stefano Gallotti <sgallotti@expediagroup.com>
2020-11-30 21:26:10 -0800

    coverage day1

--- tests/y_2020/test_2020_day1.py
+++ tests/y_2020/test_2020_day1.py
@@ -3,7 +3,9 @@ from y_2020.day1 import calculate_1, calculate_2
 
 def test_calculate_1():
     assert calculate_1([1721, 979, 366, 299, 675, 1456]) == 514579
+    assert calculate_1([]) == 0
 
 
 def test_calculate_2():
     assert calculate_2([1721, 979, 366, 299, 675, 1456]) == 241861950
+    assert calculate_2([]) == 0
