feedd439a614669bdef78fc068fb421a4efbb23f
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-03 06:40:42 -0800

    use sum

--- y_2020/day3.py
+++ y_2020/day3.py
@@ -1,3 +1,9 @@
+try:
+    from utils import prod
+except:
+    from .utils import prod
+
+
 def calculate_1(x, a=3, b=1):
     pos = (0, 0)
     c = 0
@@ -11,7 +17,6 @@ def calculate_1(x, a=3, b=1):
 
 
 def calculate_2(x):
-    i = 1
-    for a in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
-        i *= calculate_1(x, *a)
+    rules = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    i = prod([calculate_1(x, *a) for a in rules])
     return i
---
+++ y_2020/utils.py
@@ -0,0 +1,12 @@
+def sum(x):
+    c = 0
+    for i in x:
+        c += i
+    return c
+
+
+def prod(x):
+    c = 1
+    for i in x:
+        c *= i
+    return c
