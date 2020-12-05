7a8c196e754b37a68ec83502df99a878bb8eb110
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-05 15:51:48 -0800

    test day2

--- tests/y_2020/test_2020_day2.py
+++ tests/y_2020/test_2020_day2.py
@@ -1,27 +1,23 @@
-from y_2020.day2 import calculate_1, calculate_2
+from y_2020.day2 import Day
+from unittest.mock import patch, mock_open
+
+with patch("builtins.open", mock_open(read_data="")):
+    day = Day()
 
 
 def test_calculate_1():
-    assert (
-        calculate_1(
-            [
-                "1-3 a: abcde",
-                "1-3 b: cdefg",
-                "2-9 c: ccccccccc",
-            ]
-        )
-        == 2
-    )
+    day._input_data = [
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc",
+    ]
+    assert day._calculate_1() == 2
 
 
 def test_calculate_2():
-    assert (
-        calculate_2(
-            [
-                "1-3 a: abcde",
-                "1-3 b: cdefg",
-                "2-9 c: ccccccccc",
-            ]
-        )
-        == 1
-    )
+    day._input_data = [
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc",
+    ]
+    assert day.calculate_2() == 1
