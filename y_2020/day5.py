dfa81997e95b25aa9367179c44a571f124307027
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-04 21:45:56 -0800

    better var names

--- y_2020/day5.py
+++ y_2020/day5.py
@@ -1,23 +1,16 @@
 def decode_seat(x):
-    t = sum(2 ** (6 - j) for j, i in enumerate(x[:7]) if i == "B")
-    u = sum(2 ** (2 - j) for j, i in enumerate(x[7:]) if i == "R")
-    return t * 8 + u
+    row = sum(2 ** (6 - c) for c, value in enumerate(x[:7]) if value == "B")
+    column = sum(2 ** (2 - c) for c, value in enumerate(x[7:]) if value == "R")
+    return row * 8 + column
 
 
 def calculate_1(x):
-    t = 0
-    for i in x:
-        c = decode_seat(i)
-        if c > t:
-            t = c
-    return t
+    return max([decode_seat(i) for i in x])
 
 
 def calculate_2(x):
     seats = sorted([decode_seat(i) for i in x])
-    for i, j in enumerate((seats)):
-        if i == 0:
-            continue
-        if seats[i] != seats[i - 1] + 1:
-            return j - 1
+    for c, value in enumerate((seats)):
+        if seats[c] != seats[c + 1] - 1:
+            return value + 1
     return 0
