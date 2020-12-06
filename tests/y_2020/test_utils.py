0c8257284e453162e0e15354d0f1fc8e6c9b9ace
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-05 18:01:35 -0800

    add test to utils

--- tests/y_2020/test_utils.py
+++ tests/y_2020/test_utils.py
@@ -1,4 +1,4 @@
-from y_2020.utils import collapse_strings, prod
+from y_2020.utils import collapse_strings, dict_from_string, prod
 
 
 def test_prod():
@@ -10,3 +10,7 @@ def test_prod():
 def test_collapse_strings():
     assert collapse_strings(["x", "", "y1", "y2"]) == ["x", "y1 y2"]
     assert collapse_strings(["x", "y", "", "z"]) == ["x y", "z"]
+
+
+def test_dict_from_string():
+    assert dict_from_string(":") == {"": ""}
