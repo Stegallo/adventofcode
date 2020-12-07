079cdb2eab965d26ce2bd29cb7257bdf7e4e38c4
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-06 16:08:06 -0800

    uniform 2019

--- tests/y_2019/test_2019_day1.py
+++ tests/y_2019/test_2019_day1.py
@@ -1,22 +1,43 @@
-from y_2019.day1 import calculate_1, calculate_2, inner_1, inner_2
+from unittest.mock import mock_open, patch
+
+from y_2019.day1 import Day
+
+with patch("builtins.open", mock_open(read_data="")):
+    day = Day()
 
 
-def test_inner_1():
-    assert inner_1(12) == 2
-    assert inner_1(14) == 2
-    assert inner_1(1969) == 654
-    assert inner_1(100756) == 33583
+def test__preprocess_input():
+    day._input_data = [
+        "143843",
+        "144558",
+        "116244",
+    ]
+    day._preprocess_input()
+    assert day._Day__mass_list == [143843, 144558, 116244]
+
+    day._input_data = []
+    day._preprocess_input()
+    assert day._Day__mass_list == []
 
 
 def test_calculate_1():
-    assert calculate_1(["12"]) == 2
-
-
-def test_inner_2():
-    assert inner_2(14) == 2
-    assert inner_2(1969) == 966
-    assert inner_2(100756) == 50346
+    day._Day__mass_list = [12]
+    assert day._calculate_1() == 2
 
 
 def test_calculate_2():
-    assert calculate_2(["100756"]) == 50346
+    day._Day__mass_list = [100756]
+    assert day._calculate_2() == 50346
+
+
+def test_fuel_from_mass():
+    assert Day.fuel_from_mass(12) == 2
+    assert Day.fuel_from_mass(14) == 2
+    assert Day.fuel_from_mass(1969) == 654
+    assert Day.fuel_from_mass(100756) == 33583
+
+
+def test_total_fuel_from_mass():
+    assert Day.total_fuel_from_mass(14) == 2
+    assert Day.total_fuel_from_mass(1969) == 966
+    assert Day.total_fuel_from_mass(100756) == 50346
