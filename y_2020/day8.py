d719e99288fafaf1087c241c779ed1a75ebe9736
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-07 22:23:28 -0800

    refactor

--- y_2020/day8.py
+++ y_2020/day8.py
@@ -24,34 +24,42 @@ class Day(AoCDay):
             Instruction(i.split(" ")[0], int((i.split(" "))[1]))
             for i in self._input_data
         ]
-        self.__accumulator = 0
-        self.__instruction_count = defaultdict(int)
+        # self.__accumulator = 0
+        # self.__instruction_count = defaultdict(int)
 
-    def _calculate_1(self):
-        self.__accumulator = 0
-        self.__instruction_count.clear()
+    def _execute(self, local_instructions=None):
+        if not local_instructions:
+            local_instructions = self.__instructions
+        accumulator = 0
+        instruction_count = defaultdict(int)
+
+        infinite_loop = False
         instruction_pointer = 0
-        while True:
-            if self.__instruction_count[instruction_pointer] > 0:
-                break
-            self.__instruction_count[instruction_pointer] += 1
+        while instruction_pointer < len(local_instructions):
+            val = local_instructions[instruction_pointer]
 
-            current_instruction = self.__instructions[instruction_pointer]
+            if instruction_count[instruction_pointer] > 0:
+                infinite_loop = True
+                break
+            instruction_count[instruction_pointer] += 1
+
+            current_instruction = local_instructions[instruction_pointer]
             if current_instruction.operation == "acc":
-                self.__accumulator += current_instruction.argument
+                accumulator += current_instruction.argument
                 instruction_pointer += 1
             elif current_instruction.operation == "jmp":
                 instruction_pointer += current_instruction.argument
             elif current_instruction.operation == "nop":
                 instruction_pointer += 1
-        return self.__accumulator
+        if not infinite_loop:
+            return accumulator, infinite_loop
+        return accumulator, infinite_loop
+
+    def _calculate_1(self):
+        x = self._execute()
+        return x[0]
 
     def _calculate_2(self):
-        # global ACC
-        # ACC = 0
-        # global INSTR
-        # INSTR = defaultdict(int)
-        # z = [(i.split(" ")[0], i.split(" ")[1]) for i in self._input_data]
         for attempt in range(len(self.__instructions)):
             local_instructions = list(self.__instructions)
             if local_instructions[attempt].operation == "nop":
@@ -63,26 +71,6 @@ class Day(AoCDay):
                     "nop", local_instructions[attempt].argument
                 )
 
-            self.__accumulator = 0
-            self.__instruction_count.clear()
-
-            infinite_loop = False
-            instruction_pointer = 0
-            while instruction_pointer < len(local_instructions):
-                val = local_instructions[instruction_pointer]
-
-                if self.__instruction_count[instruction_pointer] > 0:
-                    infinite_loop = True
-                    break
-                self.__instruction_count[instruction_pointer] += 1
-
-                current_instruction = local_instructions[instruction_pointer]
-                if current_instruction.operation == "acc":
-                    self.__accumulator += current_instruction.argument
-                    instruction_pointer += 1
-                elif current_instruction.operation == "jmp":
-                    instruction_pointer += current_instruction.argument
-                elif current_instruction.operation == "nop":
-                    instruction_pointer += 1
-            if not infinite_loop:
-                return self.__accumulator
+            x = self._execute(local_instructions)
+            if not x[1]:
+                return x[0]
