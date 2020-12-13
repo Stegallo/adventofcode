3c4e53b551fcd4c9f2e1b418e851435c933c7bb2
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-13 15:23:33 -0800

    not working code

--- y_2020/day13.py
+++ y_2020/day13.py
@@ -10,6 +10,9 @@ class Day(AoCDay):
 
     def _preprocess_input(self):
         self.__input = [i for i in self._input_data]
+        self.__first = 0
+        self.__second = 0
+        self.__i = 0
 
     def _calculate_1(self):
         # info(self._input_data)
@@ -63,8 +66,10 @@ class Day(AoCDay):
         next_dep = [i for i in deps]
         print(f"{next_dep=}")
         intersection = [0]
+        k = 2
         while True:
-            intersection.append(find_intersection(next_dep[:2], intersection[-1]))
+            intersection.append(self.find_intersection(next_dep[:k], intersection[-1]))
+            k += 1
             # print(f"{intersection=}")
             # print(f"{intersection[-1]=}")
             current_time = intersection[-1]
@@ -129,35 +134,105 @@ class Day(AoCDay):
         # return next_dep[0]
         return intersection[-1]
 
+    def find_intersection(self, dep, time=0):
+        print(f"")
+        print(f"{dep=}")
+        print(f"{self.__first=}")
+        print(f"{self.__second=}")
+        print(f"{time=}, {self.__i=}")
+        if time > 0:
+            map_f = {}
+            map_s = {}
+            for j in range(len(dep)):
+                if dep[j]:
+                    map_f[j] = (self.__first + j) / dep[j]
+                    map_s[j] = (self.__second + j) / dep[j]
+                    print(
+                        f"{j=},{(self.__second +j)/ dep[j] - (self.__first +j)/ dep[j]=}"
+                    )
+                    print(f"{(self.__first +j) / dep[j]=}")
+                    print(f"{(self.__second +j)/ dep[j]=}")
+            print()
+            print(f"{map_f=}")
+            print(f"{map_s=}")
+            pre_return = []
+            for j in range(len(dep)):
+                if dep[j]:
+                    print(f"{map_f[j]=}")
+                    print(f"{map_s[j]=}")
+                    print(map_f[j] + (map_s[j] - map_f[j]) * self.__i)
+                    pre_return.append(map_f[j] + (map_s[j] - map_f[j]) * self.__i)
+            print(f"{pre_return=}")
+            print(f"{pre_return[0] * dep[0]=}")
+            self.__i += 1
+            print(pre_return)
+            # breakpoint()
+            return pre_return[0] * dep[0]
+            # print(f"{self.__second / dep[0]=}")
+            # print(f"{self.__first / dep[0]=}")
+            # print(f"{self.__second / dep[0] - self.__first / dep[0]=}")
+            # print(f"{(self.__second +1)/ dep[1] - (self.__first +1)/ dep[1]=}")
 
-def find_intersection(dep, time=0):
-    # print(f"{dep=}")
-    next_dep = [i for i in dep]
-    # time = 0
-    increment = min([i for i in next_dep if i])
-    while True:
-        for i in range(len(dep)):
-            # print(f"{next_dep[i]=}, {time=}")
-            if dep[i]:
-                while (next_dep[i]) < time + 1:
-                    next_dep[i] += dep[i]
+        next_dep = [i for i in dep]
+        # time = 0
+        increment = next_dep[0]  # min([i for i in next_dep if i])
+        while True:
+            # breakpoint()
+            for i in range(len(dep)):
+                # print(f"{next_dep[i]=}, {time=}")
+                if dep[i]:
+                    while (next_dep[i]) < time + increment:
+                        next_dep[i] += dep[i]
 
-        #     # next_dep = [1068781, 1068782, 0, 0, 1068785, 0, 1068787, 1068788]
-        #     # time = 1068781
-        solved = True
-        # print(f"{next_dep=}")
-        # breakpoint()
-        for c, i in enumerate(next_dep):
-            # print(f"{time=},{next_dep=}, {next_dep[0] + c=}, {i=}")
-            if next_dep[0] + c != i:
-                if i is not None:
-                    solved = False
-                    break
-                # time = 1068781
-            # print(f"{c}, {i}")
-            # print(f"{solved=}")
+            #     # next_dep = [1068781, 1068782, 0, 0, 1068785, 0, 1068787, 1068788]
+            #     # time = 1068781
+            solved = True
+            # print(f"{next_dep=}")
+            # breakpoint()
+            for c, i in enumerate(next_dep):
+                # print(f"{time=},{next_dep=}, {next_dep[0] + c=}, {i=}")
+                if next_dep[0] + c != i:
+                    if i is not None:
+                        solved = False
+                        break
+                    # time = 1068781
+                # print(f"{c}, {i}")
+                # print(f"{solved=}")
 
-        if solved:
-            break
+            if solved:  # or time > 754050:
+                break
+            time += increment
         time += increment
-    return time + increment
+        first_intersection = time
+        print(f"first intersection={time=}")
+        while True:
+            for i in range(len(dep)):
+                # print(f"{next_dep[i]=}, {time=}")
+                if dep[i]:
+                    while (next_dep[i]) < time + increment:
+                        next_dep[i] += dep[i]
+
+            #     # next_dep = [1068781, 1068782, 0, 0, 1068785, 0, 1068787, 1068788]
+            #     # time = 1068781
+            solved = True
+            # print(f"{next_dep=}")
+            # breakpoint()
+            for c, i in enumerate(next_dep):
+                # print(f"{time=},{next_dep=}, {next_dep[0] + c=}, {i=}")
+                if next_dep[0] + c != i:
+                    if i is not None:
+                        solved = False
+                        break
+                    # time = 1068781
+                # print(f"{c}, {i}")
+                # print(f"{solved=}")
+
+            if solved:
+                break
+            time += increment
+        time += increment
+        print(f"second intersection={time=}")
+        self.__first = first_intersection
+        self.__second = time
+        self.__i = 1
+        return first_intersection
