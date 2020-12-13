1e08bddf9572481b2597b5c9614558cc8f25b479
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-13 12:49:51 -0800

    with intersections

--- y_2020/day13.py
+++ y_2020/day13.py
@@ -61,39 +61,103 @@ class Day(AoCDay):
         ]
 
         next_dep = [i for i in deps]
-        increment = min([i for i in next_dep if i])
-        print(f"{next_dep=}, {increment}")
-        mark = 10 * 1000 * 1000
+        print(f"{next_dep=}")
+        intersection = [0]
         while True:
-            for i in range(len(deps)):
-                # print(f"{next_dep[i]=}, {time=}")
-                if deps[i]:
-                    if (next_dep[i]) < time + 1:
-                        next_dep[i] += deps[i]
-            # next_dep = [1068781, 1068782, 0, 0, 1068785, 0, 1068787, 1068788]
-            # time = 1068781
+            intersection.append(find_intersection(next_dep[:2], intersection[-1]))
+            # print(f"{intersection=}")
+            # print(f"{intersection[-1]=}")
+            current_time = intersection[-1]
+            # breakpoint()
             solved = True
-
-            for c, i in enumerate(next_dep):
-                print(f"{time=},{next_dep=}, {next_dep[0] + c=}, {i=}")
-                if next_dep[0] + c != i:
-                    if i is not None:
+            for c, i in enumerate(next_dep[:]):
+                if current_time > 350:
+                    # breakpoint()
+                    ...
+                # print(f"{c=}, {i=}")
+                if i:
+                    # print(f"{(current_time+c)%i=}, {i=}")
+                    if (current_time + c) % i != 0:
+                        # print("breakin")
                         solved = False
                         break
-                    # time = 1068781
-                # print(f"{c}, {i}")
-                # print(f"{solved=}")
 
-            # if solved or time > 1000:  # 1068790:
-            # if solved or time > 1068790:
-            if solved:  # or
-                # if time > 90:
-                # if True:
+                # print(f"{solved=}")
+            if solved:
+                # intersection.append(find_intersection(next_dep[:2], intersection[-1]))
+                # print(f"{intersection=}")
+                # print(f"{intersection[-1]=}")
                 break
-            print(f"{time=},{next_dep=},{increment=}\n")
-            if time > mark:
-                print(f"{time=}, {mark=}")
-                mark = mark + 10 * 1000 * 1000
-            time += increment
-        print(f"\n{next_dep=}, {time + increment=}")
-        return next_dep[0]
+        # for i in next_dep:
+        #     print(f"{i=}")
+        # increment = min([i for i in next_dep if i])
+        # print(f"{next_dep=}, {increment}")
+
+        # mark = 10 * 1000 * 1000
+        # while True:
+        #     for i in range(len(deps)):
+        #         # print(f"{next_dep[i]=}, {time=}")
+        #         if deps[i]:
+        #             if (next_dep[i]) < time + 1:
+        #                 next_dep[i] += deps[i]
+        #     # next_dep = [1068781, 1068782, 0, 0, 1068785, 0, 1068787, 1068788]
+        #     # time = 1068781
+        #     solved = True
+        #
+        #     for c, i in enumerate(next_dep):
+        #         print(f"{time=},{next_dep=}, {next_dep[0] + c=}, {i=}")
+        #         if next_dep[0] + c != i:
+        #             if i is not None:
+        #                 solved = False
+        #                 break
+        #             # time = 1068781
+        #         # print(f"{c}, {i}")
+        #         # print(f"{solved=}")
+        #
+        #     # if solved or time > 1000:  # 1068790:
+        #     # if solved or time > 1068790:
+        #     if solved:  # or
+        #         # if time > 90:
+        #         # if True:
+        #         break
+        #     print(f"{time=},{next_dep=},{increment=}\n")
+        #     if time > mark:
+        #         print(f"{time=}, {mark=}")
+        #         mark = mark + 10 * 1000 * 1000
+        #     time += increment
+        # print(f"\n{next_dep=}, {time + increment=}")
+        # return next_dep[0]
+        return intersection[-1]
+
+
+def find_intersection(dep, time=0):
+    # print(f"{dep=}")
+    next_dep = [i for i in dep]
+    # time = 0
+    increment = min([i for i in next_dep if i])
+    while True:
+        for i in range(len(dep)):
+            # print(f"{next_dep[i]=}, {time=}")
+            if dep[i]:
+                while (next_dep[i]) < time + 1:
+                    next_dep[i] += dep[i]
+
+        #     # next_dep = [1068781, 1068782, 0, 0, 1068785, 0, 1068787, 1068788]
+        #     # time = 1068781
+        solved = True
+        # print(f"{next_dep=}")
+        # breakpoint()
+        for c, i in enumerate(next_dep):
+            # print(f"{time=},{next_dep=}, {next_dep[0] + c=}, {i=}")
+            if next_dep[0] + c != i:
+                if i is not None:
+                    solved = False
+                    break
+                # time = 1068781
+            # print(f"{c}, {i}")
+            # print(f"{solved=}")
+
+        if solved:
+            break
+        time += increment
+    return time + increment
