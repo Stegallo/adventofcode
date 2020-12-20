ff2d79c22b07e3bf9966bc49c97b7afd06f16a29
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-20 11:34:18 -0800

    day 18

--- tests/y_2020/test_2020_day18.py
+++ tests/y_2020/test_2020_day18.py
@@ -25,31 +25,31 @@ def test_calculate_1():
     assert day._calculate_1() == 437
     day._Day__input = ["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"]
     assert day._calculate_1() == 12240
-    # day._Day__input = ["((2 + 3 * 4) * (5 + 6 * 7 + 8) + 9) + 10 + 11 * 12"]
-    # assert day._calculate_1() == 0
+    day._Day__input = ["((2 + 3 * 4) * (5 + 6 * 7 + 8) + 9) + 4 + 5 * 6"]
+    assert day._calculate_1() == 10308
     day._Day__input = ["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
     assert day._calculate_1() == 13632
 
 
-# ((5*4)*(11*15)+9)+10+11*12
-# (20*165+9)+10+11*12
-# (20*174)+10+11*12
-# 3480+10+11*12
-# 3501*12
-# 42012
+# ((5*4)*(11*15)+9)+4+5*6
+# (20*165+9)+4+5*6
+# (20*174)+4+5*6
+# 3480+4+5*6
+# 3489*6
+# 20934
 def test_calculate_2():
     print()
-    # day._Day__input = ["1 + 2 * 3 + 4 * 5 + 6"]
-    # assert day._calculate_2() == 231
-    # day._Day__input = ["1 + (2 * 3) + (4 * (5 + 6))"]
-    # assert day._calculate_1() == 51
-    # day._Day__input = ["2 * 3 + (4 * 5)"]
-    # assert day._calculate_1() == 46
-    # day._Day__input = ["5 + (8 * 3 + 9 + 3 * 4 * 3)"]
-    # assert day._calculate_1() == 1445
-    # day._Day__input = ["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"]
-    # assert day._calculate_1() == 669060
-    day._Day__input = ["((2 + 3 * 4) * (5 + 6 * 7 + 8) + 9) + 10 + 11 * 12"]
-    assert day._calculate_1() == 0
-    # day._Day__input = ["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
-    # assert day._calculate_1() == 23340
+    day._Day__input = ["1 + 2 * 3 + 4 * 5 + 6"]
+    assert day._calculate_2() == 231
+    day._Day__input = ["1 + (2 * 3) + (4 * (5 + 6))"]
+    assert day._calculate_2() == 51
+    day._Day__input = ["2 * 3 + (4 * 5)"]
+    assert day._calculate_2() == 46
+    day._Day__input = ["5 + (8 * 3 + 9 + 3 * 4 * 3)"]
+    assert day._calculate_2() == 1445
+    day._Day__input = ["5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"]
+    assert day._calculate_2() == 669060
+    day._Day__input = ["((2 + 3 * 4) * (5 + 6 * 7 + 8) + 9) + 4 + 5 * 6"]
+    assert day._calculate_2() == 20934
+    day._Day__input = ["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
+    assert day._calculate_2() == 23340
--- y_2020/day18.py
+++ y_2020/day18.py
@@ -24,10 +24,65 @@ class Day(AoCDay):
     def _preprocess_input(self):
         self.__input = [i for i in self._input_data]
 
-    def calc(self, lst):
+    def calc1(self, lst):
         # global GLOBAL_POINTER
         # print(lst)
-        print()
+        # print()
+        result = 0
+        op = None
+        op_n = 0
+        a = 0
+        b = 0
+        x = []
+        ignore = 0
+        # breakpoint()
+        for c, j in enumerate(lst):
+            # print(f"{j=} in {''.join(lst)}, {ignore=}; {x=}")
+            if ignore:
+                if j == ")":
+                    ignore -= 1
+                if j == "(":
+                    ignore += 1
+                continue
+            if j == ")":
+                return a
+            # continue
+            if j in OPS:
+                # print(OPS)
+                op = j
+            else:
+                if op_n == 0:
+                    if j == "(":
+                        ignore += 1
+                        # breakpoint()
+                        a = self.calc1(lst[c + 1 :])
+                    else:
+                        a = int(j)
+                    x.append(a)
+                    op_n = 1
+                else:
+                    if j == "(":
+                        ignore += 1
+                        # breakpoint()
+                        b = self.calc1(lst[c + 1 :])
+                    else:
+                        b = int(j)
+                    if op == "+" or True:
+                        # a = x[-1]
+                        # print(f"{a=}, {b=}")
+                        a = OPS[op](a, b)
+                        # x[-1] = a
+                    else:
+                        x.append(b)
+                        # breakpoint()
+                # print(f"{x=}")
+
+        return a
+
+    def calc2(self, lst):
+        # global GLOBAL_POINTER
+        # print(lst)
+        # print()
         result = 0
         op = None
         op_n = 0
@@ -55,7 +110,7 @@ class Day(AoCDay):
                     if j == "(":
                         ignore += 1
                         # breakpoint()
-                        a = self.calc(lst[c + 1 :])
+                        a = self.calc2(lst[c + 1 :])
                     else:
                         a = int(j)
                     x.append(a)
@@ -64,7 +119,7 @@ class Day(AoCDay):
                     if j == "(":
                         ignore += 1
                         # breakpoint()
-                        b = self.calc(lst[c + 1 :])
+                        b = self.calc2(lst[c + 1 :])
                     else:
                         b = int(j)
                     if op == "+":
@@ -80,21 +135,21 @@ class Day(AoCDay):
         return prod(x)
 
     def _calculate_1(self):
-        print(self.__input)
+        # print(self.__input)
         result = 0
         for i in self.__input:
             express = [k for k in list(i) if k != " "]
-            print(f"{express=}")
-            result += self.calc(express)
+            # print(f"{express=}")
+            result += self.calc1(express)
 
         return result
 
     def _calculate_2(self):
-        print(self.__input)
+        # print(self.__input)
         result = 0
         for i in self.__input:
             express = [k for k in list(i) if k != " "]
-            print(f"{express=}")
-            result += self.calc(express)
+            # print(f"{express=}")
+            result += self.calc2(express)
 
         return result
