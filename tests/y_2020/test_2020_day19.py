ce41e0d01adf79be046043d07a1ef179547f4d28
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-27 10:44:07 -0800

    day 19 not working yet

--- tests/y_2020/test_2020_day19.py
+++ tests/y_2020/test_2020_day19.py
@@ -2,21 +2,96 @@ from unittest.mock import mock_open, patch
 
 from y_2020.day19 import Day
 
-with patch("builtins.open", mock_open(read_data="")):
+with patch("builtins.open", mock_open(read_data="0: 0")):
     day = Day()
 
 
 def test__preprocess_input():
     print()
-    day._input_data = []
+    day._input_data = ["0: 0", "", "a"]
     day._preprocess_input()
-    assert day._Day__input == []
+    assert day._Day__rules == {"0": '0 "c"'}
+    assert day._Day__messages == ["a"]
+
+
+def test_is_valid():
+    print()
+    day._Day__rules = {
+        "0": '"a"',
+    }
+    assert day.is_valid("a") == True
+    assert day.is_valid("b") == False
+
+    day._Day__rules = {
+        "0": '"b"',
+    }
+    assert day.is_valid("b") == True
+
+    day._Day__rules = {
+        "0": "1",
+        "1": '"a"',
+    }
+    assert day.is_valid("a") == True
+    assert day.is_valid("b") == False
+
+    day._Day__rules = {
+        "0": '"b" 1',
+        "1": '"a"',
+    }
+    assert day.is_valid("b") == False
+    assert day.is_valid("ba") == True
+    assert day.is_valid("bb") == False
+    assert day.is_valid("bba") == False
+
+    day._Day__rules = {
+        "0": '"a" | "b"',
+    }
+    assert day.is_valid("a") == True
+    assert day.is_valid("b") == True
+
+    day._Day__rules = {
+        "0": "4 1 5",
+        "1": "4 5",
+        "4": '"a"',
+        "5": '"b"',
+    }
+    assert day.is_valid("a") == False
+    assert day.is_valid("b") == False
+    assert day.is_valid("ab") == False
+    assert day.is_valid("aabb") == True
+    assert day.is_valid("aaab") == False
+    assert day.is_valid("abbb") == False
+
+    day._Day__rules = {
+        "0": "4 1 5",
+        "1": "2 3",
+        "2": "4 4",
+        "3": "4 5",
+        "4": '"a"',
+        "5": '"b"',
+    }
+
+    assert day.is_valid("aaaabb") == True
+
+    day._Day__rules = {
+        "0": '4 1 5 "c"',
+        "1": "2 3 | 3 2",
+        "2": "4 4 | 5 5",
+        "3": "4 5 | 5 4",
+        "4": '"a"',
+        "5": '"b"',
+    }
+
+    assert day.is_valid("ababbbc") == True
+    assert day.is_valid("bababac") == False
+    assert day.is_valid("abbbabc") == True
+    assert day.is_valid("aaabbbc") == False
+    assert day.is_valid("aaaabbbc") == False
 
 
 def test_calculate_1():
     print()
-
-    day._Day__input = [
+    day._input_data = [
         "0: 4 1 5",
         "1: 2 3",
         "2: 4 4",
@@ -27,9 +102,10 @@ def test_calculate_1():
         "aaaabb",
         "aaaabb",
     ]
+    day._preprocess_input()
     assert day._calculate_1() == 2
 
-    day._Day__input = [
+    day._input_data = [
         "0: 4 1 5",
         "1: 2 3 | 3 2",
         "2: 4 4 | 5 5",
@@ -46,7 +122,147 @@ def test_calculate_1():
     assert day._calculate_1() == 2
 
 
-def test_calculate_2():
+def test_calculate_2_logic1():
+    return
     print()
-    day._Day__input = []
-    assert day._calculate_2() == 0
+    day._Day__input = [
+        "42: 9 14 | 10 1",
+        "9: 14 27 | 1 26",
+        "10: 23 14 | 28 1",
+        '1: "a"',
+        "11: 42 31",
+        "5: 1 14 | 15 1",
+        "19: 14 1 | 14 14",
+        "12: 24 14 | 19 1",
+        "16: 15 1 | 14 14",
+        "31: 14 17 | 1 13",
+        "6: 14 14 | 1 14",
+        "2: 1 24 | 14 4",
+        "0: 8 11",
+        "13: 14 3 | 1 12",
+        "15: 1 | 14",
+        "17: 14 2 | 1 7",
+        "23: 25 1 | 22 14",
+        "28: 16 1",
+        "4: 1 1",
+        "20: 14 14 | 1 15",
+        "3: 5 14 | 16 1",
+        "27: 1 6 | 14 18",
+        '14: "b"',
+        "21: 14 1 | 1 14",
+        "25: 1 1 | 1 14",
+        "22: 14 14",
+        "8: 42",
+        "26: 14 22 | 1 20",
+        "18: 15 15",
+        "7: 14 5 | 1 21",
+        "24: 14 1",
+        "",
+        "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+        "bbabbbbaabaabba",
+        "babbbbaabbbbbabbbbbbaabaaabaaa",
+        "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+        "bbbbbbbaaaabbbbaaabbabaaa",
+        "bbbababbbbaaaaaaaabbababaaababaabab",
+        "ababaaaaaabaaab",
+        "ababaaaaabbbaba",
+        "baabbaaaabbaaaababbaababb",
+        "abbbbabbbbaaaababbbbbbaaaababb",
+        "aaaaabbaabaaaaababaa",
+        "aaaabbaaaabbaaa",
+        "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+        "babaaabbbaaabaababbaabababaaab",
+        "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
+    ]
+    assert day._calculate_1() == 3
+
+
+def test_calculate_2_easy():
+    return
+    print()
+    day._Day__input = [
+        "0: 8",
+        '1: "a"',
+        "6: 14 14 | 1 14",
+        "8: 42",
+        "9: 14 27 | 1 26",
+        "10: 23 14 | 28 1",
+        '14: "b"',
+        "15: 1 | 14",
+        "16: 15 1 | 14 14",
+        "18: 15 15",
+        "20: 14 14 | 1 15",
+        "22: 14 14",
+        "23: 25 1 | 22 14",
+        "25: 1 1 | 1 14",
+        "26: 14 22 | 1 20",
+        "27: 1 6 | 14 18",
+        "28: 16 1",
+        "42: 9 14 | 10 1",
+        "11: 42 31",
+        "",
+        "a",
+        "aa",
+        "aaa",
+        "aaaa",
+        "aaaaa",
+        "aaaaaa",
+        "b",
+    ]
+    assert day._calculate_2() == 2
+
+
+def test_calculate_2():
+    return
+    print()
+    day._Day__input = [
+        "42: 9 14 | 10 1",
+        "9: 14 27 | 1 26",
+        "10: 23 14 | 28 1",
+        '1: "a"',
+        "11: 42 31",
+        "5: 1 14 | 15 1",
+        "19: 14 1 | 14 14",
+        "12: 24 14 | 19 1",
+        "16: 15 1 | 14 14",
+        "31: 14 17 | 1 13",
+        "6: 14 14 | 1 14",
+        "2: 1 24 | 14 4",
+        "0: 8 11",
+        "13: 14 3 | 1 12",
+        "15: 1 | 14",
+        "17: 14 2 | 1 7",
+        "23: 25 1 | 22 14",
+        "28: 16 1",
+        "4: 1 1",
+        "20: 14 14 | 1 15",
+        "3: 5 14 | 16 1",
+        "27: 1 6 | 14 18",
+        '14: "b"',
+        "21: 14 1 | 1 14",
+        "25: 1 1 | 1 14",
+        "22: 14 14",
+        "8: 42",
+        "26: 14 22 | 1 20",
+        "18: 15 15",
+        "7: 14 5 | 1 21",
+        "24: 14 1",
+        "",
+        "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+        "bbabbbbaabaabba",  # original
+        "babbbbaabbbbbabbbbbbaabaaabaaa",  # v
+        "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",  # v
+        "bbbbbbbaaaabbbbaaabbabaaa",  # v
+        "bbbababbbbaaaaaaaabbababaaababaabab",  # v
+        "ababaaaaaabaaab",  # original
+        "ababaaaaabbbaba",  # original
+        "baabbaaaabbaaaababbaababb",  # v
+        "abbbbabbbbaaaababbbbbbaaaababb",  # v
+        "aaaaabbaabaaaaababaa",  # v
+        "aaaabbaaaabbaaa",
+        "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",  # v
+        "babaaabbbaaabaababbaabababaaab",
+        "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",  # v
+    ]
+    # assert day._calculate_2() == 1
+    assert day._calculate_2() == 12
--- y_2020/day19.py
+++ y_2020/day19.py
@@ -1,8 +1,6 @@
 import re
-
+from collections import defaultdict
 from .common import AoCDay
-
-p = 0
 
 
 class Day(AoCDay):
@@ -11,96 +9,147 @@ class Day(AoCDay):
 
     def _preprocess_input(self):
         self.__input = [i for i in self._input_data]
+        self.__rules = {}
+        self.__messages = []
+        self.get_rules()
+        self.get_messages()
+        # print(f"{self.messages=}")
+        # add terminator
+        self.__rules["0"] = self.__rules["0"] + ' "c"'
+        # self.invalid_msgs = {}
+        self.cache = defaultdict(dict)
 
-    def validate(self, rule=0):
+    def get_rules(self):
+        for i in self.__input:
+            if i == "":
+                break
+            r = i.split(": ")
+            self.__rules[(r[0])] = r[1]
 
-        # print()
-        # print(f"{self.message=}, {rule=}, {self.p=}, {self.rules[rule].split(' | ')=}")
-        # print(self.rules)
-        # print(self.rules[rule].split(" "))
-        # breakpoint()
-        outer_valid = False
-        temp_p = self.p
-        for j in self.rules[rule].split(" | "):
-            valid = True
-            self.p = temp_p
-            for i in j.split(" "):
-                # breakpoint()
-                # print(f"{int(i)=}")
-                # print(f"{self.rules[int(i)]=}")
-                int_i = None
-                str_i = None
-                try:
-                    int_i = int(i)
-                except ValueError as e:
-                    ...
-                if int_i:
-                    # print("rule is other rule")
-                    if not self.validate(int_i):
-                        valid = False
-                    # print(f"{p=},{valid=}")
-                    # self.p += 1
-                else:
-                    # print("rule is char")
-                    # raise e
-                    # breakpoint()
-                    str_i = i.replace('"', "")
-                    # print(f"{self.p=},{str_i=},{self.message[self.p]=}")
-                    if self.message[self.p] != str_i:
-                        valid = False
-                    # else:
-                    #     valid = False
-                    self.p += 1
-            # print(
-            #     f"{self.rules[rule]}, {self.p=}, self.message[self.p]=, {valid=}, {len(self.message)=}"
-            # )
-            if valid:
-                outer_valid = True
-        if rule == 0 and len(self.message) > self.p:
+    def get_messages(self):
+        found = False
+        for i in self.__input:
+            if i == "":
+                found = True
+                continue
+            if found:
+                self.__messages.append(i)
+
+    def match_singe_rule(self, msg, rule):
+        return False
+        print(f"{msg=}, {rule=}")
+        if len(msg) < len(rule):
+            print("msg shorter")
             return False
-        return outer_valid
+        if len(msg) == len(rule) == 0:
+            print("all zero")
+            return True
+        if len(rule) == 0:
+            print("len rule is zero")
+            return False
+        for c, i in enumerate(msg):
+            prefix = msg[: c + 1]
+            first_rule = rule[0]
+            print(f"{prefix=}, {first_rule=}")
+            valid = self.is_valid(prefix, first_rule)
+            if valid:
+                print(f"{prefix=}, {first_rule=}, {valid=}, {rule=}, {len(rule)=}")
+                if self.match_singe_rule(msg[c + 1 :], rule[1:]):
+                    return True
+        return False
+
+    def _is_valid(self, msg, rule="0"):
+        return False
+        print(f"{msg=}, {self.invalid_msgs=}")
+        if msg in self.invalid_msgs:
+            if rule in self.invalid_msgs[msg]:
+                return False
+        for alternative in (
+            self.rules[rule].replace(" ", "").replace('"', "").split("|")
+        ):
+            print(f"{msg=}; {alternative=}")
+            # continue
+            if msg in alternative:
+                return True
+            if alternative in ("a", "b"):
+                if msg in self.invalid_msgs:
+                    self.invalid_msgs[msg].append(rule)
+                else:
+                    self.invalid_msgs[msg] = []
+                return False
+            if self.match_singe_rule(msg, alternative):
+                print(f"{msg=} is valid! for {rule=}, {alternative=}")
+                return True
+        if msg in self.invalid_msgs:
+            self.invalid_msgs[msg].append(rule)
+        else:
+            self.invalid_msgs[msg] = []
+        return False
+
+    def is_valid(self, msg, rule_list=["0"]):
+        # breakpoint()
+        # print(f"\n{rule_list=} ... {msg=}")
+        if len(rule_list) == 1:
+            if rule_list[0] in ["a", "b", "c"]:
+                return msg == rule_list[0]
+            alternative = "".join(
+                self.__rules[rule_list[0]].replace('"', "").split(" ")
+            )
+            if self.cache.get(msg, {}).get(alternative) is True:
+                valid = True
+            elif self.cache.get(msg, {}).get(alternative) is False:
+                valid = False
+            else:
+                valid = self.is_valid(msg, alternative)
+                self.cache[msg][alternative] = valid
+            return valid
+        alternative_valid = False
+        for alternative in "".join(rule_list).split("|"):
+            # print(f"{self.cache=}")
+            # print(f"{msg}; {self.cache.get(msg, {}).get(alternative)=}")
+            if self.cache.get(msg, {}).get(alternative) is True:
+                alternative_valid = True
+                break
+            if self.cache.get(msg, {}).get(alternative) is False:
+                continue
+            # print(f"{alternative=}")
+            # print(f"{alternative=}, {msg}")
+            original_msg = msg
+            if len(alternative) > len(msg):
+                continue
+            # print(f"{alternative=}, {msg}")
+            valid = True
+            for rule_key in alternative:
+                # print(f"{rule_key=}, {msg=}")
+                # breakpoint()
+                inner_valid = False
+                for i in range(len(msg)):
+
+                    if self.is_valid(msg[: i + 1], [rule_key]):
+                        # print(f"{valid=}")
+                        inner_valid = True
+                        msg = msg[i + 1 :]
+                        break
+                        # print(f"{valid=}")
+                if not inner_valid:
+                    valid = False
+                    break
+            if valid:
+                alternative_valid = True
+                self.cache[original_msg][alternative] = True
+                break
+            self.cache[original_msg][alternative] = False
+
+        return alternative_valid
 
     def _calculate_1(self):
-        # info(self._input_data)
-        # print(*self._input_data, sep="\n")
-        # y = [int(i) for i in self._input_data.split(",")]
-        # print(sum(y))
-        self.rules = {}
-        end_rules = False
+        # print(f"{self.rules=}")
         c = 0
-        for i in self.__input:
-            if not end_rules:
-                # print(f"{i=}")
-                if i == "":
-                    end_rules = True
-                    continue
-                r = i.split(": ")
-                # print(f"{r=}")
-                self.rules[int(r[0])] = r[1]
-                # print(f"{rules=}")
-            if end_rules:
-                # print(f"{i=}")
-                self.message = i
-                self.p = 0
-                c += self.validate()
-                # break
+        breakpoint()
+        for i in self.__messages:
+            c += self.is_valid(i + "c")
         return c
 
     def _calculate_2(self):
-        self.__input
-        return 0
-
-
-def info(x):
-    print(f"{len(x)=}")
-    hf = len(x) // 2 + 1
-    try:
-        print(f"{x[+0]=}")
-        print(f"{x[hf]=}")
-        print(f"{x[-1]=}")
-    except:
         ...
-
-    # regex = "([\d]+)-([\d]+) ([\D]): ([\D]*)$"
-    # fa = re.findall(regex, x[0])[0]
-    # # print(*fa, sep="\n")
+        return 0
