642d6a174179c2019380262bd5013232a3cef5d8
Stefano Gallotti <sgallotti@expediagroup.com>
2020-12-29 08:00:32 -0800

    day21

--- tests/y_2020/test_2020_day21.py
+++ tests/y_2020/test_2020_day21.py
@@ -8,20 +8,50 @@ with patch("builtins.open", mock_open(read_data="")):
 
 def test__preprocess_input():
     print()
-    day._input_data = []
+    day._input_data = [
+        "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+        "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+        "sqjhc fvjkl (contains soy)",
+        "sqjhc mxmxvkd sbzzf (contains fish)",
+    ]
     day._preprocess_input()
-    assert day._Day__input == []
+    assert day._Day__recipes == [
+        [["mxmxvkd", "kfcds", "sqjhc", "nhms"], ["dairy", "fish"]],
+        [["trh", "fvjkl", "sbzzf", "mxmxvkd"], ["dairy"]],
+        [["sqjhc", "fvjkl"], ["soy"]],
+        [["sqjhc", "mxmxvkd", "sbzzf"], ["fish"]],
+    ]
+    assert day._Day__possible_allers == {
+        "kfcds": {"dairy", "fish"},
+        "mxmxvkd": {"dairy", "fish"},
+        "nhms": {"dairy", "fish"},
+        "sqjhc": {"dairy", "soy", "fish"},
+        "trh": {"dairy"},
+        "sbzzf": {"dairy", "fish"},
+        "fvjkl": {"dairy", "soy"},
+    }
+    assert day._Day__recipes_with == {"dairy": [0, 1], "fish": [0, 3], "soy": [2]}
 
 
 def test_calculate_1():
     print()
-    day._input_data = []
+    day._input_data = [
+        "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+        "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+        "sqjhc fvjkl (contains soy)",
+        "sqjhc mxmxvkd sbzzf (contains fish)",
+    ]
     day._preprocess_input()
-    assert day._calculate_1() == 0
+    assert day._calculate_1() == 5
 
 
 def test_calculate_2():
     print()
-    day._input_data = []
+    day._input_data = [
+        "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+        "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+        "sqjhc fvjkl (contains soy)",
+        "sqjhc mxmxvkd sbzzf (contains fish)",
+    ]
     day._preprocess_input()
-    assert day._calculate_2() == 0
+    assert day._calculate_2() == "mxmxvkd,sqjhc,fvjkl"
--- y_2020/day21.py
+++ y_2020/day21.py
@@ -1,5 +1,7 @@
 import re
 from .common import AoCDay
+from collections import defaultdict
+from copy import deepcopy
 
 
 class Day(AoCDay):
@@ -7,10 +9,79 @@ class Day(AoCDay):
         super().__init__(21)
 
     def _preprocess_input(self):
-        self.__input = [i for i in self._input_data]
+        result = []
+        for i in self._input_data:
+            x = re.findall("(.*) \(contains (.*)\)", i)[0]
+            result.append([x[0].split(" "), x[1].split(", ")])
+
+        self.__recipes = result
+        self.__possible_allers = defaultdict(set)
+        self.__recipes_with = defaultdict(list)
+
+        for c, i in enumerate(self.__recipes):
+            allergens = set(i[1])
+            for aller in allergens:
+                self.__recipes_with[aller].append(c)
+            for ingr in i[0]:
+                self.__possible_allers[ingr] |= allergens
+
+        self.__safe = self.safe_ingredients()
+
+    def safe_ingredients(self):
+        safe = []
+
+        for ingr, possible in (self.__possible_allers).items():
+            # possible = self.__possible_allers[ingr]
+            impossible = set()
+            # breakpoint()
+
+            for aller in possible:
+                for i in self.__recipes_with[aller]:
+                    if ingr not in self.__recipes[i][0]:
+                        impossible.add(aller)
+                        break
+
+            # Difference between sets: remove all the items of `impossible` from `possible`
+            possible -= impossible
+
+            # If no possible allergens are left, the ingredient does not contain allergens
+            if not possible:
+                safe.append(ingr)
+
+        return safe
+
+    # safe = safe_ingredients(recipes, possible_allers, recipes_with)
 
     def _calculate_1(self):
-        return 0
+        print(f"{self.__possible_allers=}")
+        return sum(ingr in r[0] for r in self.__recipes for ingr in self.__safe)
+
+    def simplify(self):
+        assigned = {}
+
+        while self.__possible_allers:
+            for ingr, possible in self.__possible_allers.items():
+                if len(possible) == 1:
+                    break
+
+            aller = possible.pop()
+            assigned[aller] = ingr
+            del self.__possible_allers[ingr]
+
+            for ingr, possible in self.__possible_allers.items():
+                if aller in possible:
+                    possible.remove(aller)
+
+        return assigned
 
     def _calculate_2(self):
-        return 0
+        print(f"{self.__possible_allers=}")
+        print(f"{self.__safe=}")
+        for ingr in self.__safe:
+            del self.__possible_allers[ingr]
+
+        assigned = self.simplify()
+        print(f"{sorted(assigned)=}")
+        print(f"{assigned=}")
+        lst = ",".join(map(assigned.get, sorted(assigned)))
+        return lst
