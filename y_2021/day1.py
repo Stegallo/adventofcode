fa9458cc6687391f45da60d5b8688f13347dff39
Stefano Gallotti <sgallotti@expediagroup.com>
2021-12-04 18:29:46 -0800

    day1 clean

--- y_2021/day1.py
+++ y_2021/day1.py
@@ -10,23 +10,9 @@ class Day(AoCDay):
 
     def _calculate_1(self):
         data = self.__input_data
-        previous = 100000
-        result = 0
-        for value in data:
-            if value > previous:
-                result += 1
-            previous = value
-        return result
+
+        return sum([data[index] < value for index, value in enumerate(data[1:])])
 
     def _calculate_2(self):
         data = self.__input_data
-        result = 0
-        previous = 100000
-        for index, value in enumerate(data):
-            if index < 2:
-                continue
-            sum = data[index] + data[index - 1] + data[index - 2]
-            if sum > previous:
-                result += 1
-            previous = sum
-        return result
+        return sum([data[index] < value for index, value in enumerate(data[3:])])
