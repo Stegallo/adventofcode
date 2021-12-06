426565bcd057102c88bfa25459ea664466881102
Stefano Gallotti <sgallotti@expediagroup.com>
2021-12-05 18:23:31 -0800

    precommit

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,7 @@
 set -e
 
+python3 -m pip -q install pre-commit
+pre-commit install
+
 docker build -f docker/Dockerfile -t $(basename $(pwd)) .
 docker run -it --rm -w /app -v $(pwd):/app --net host $(basename $(pwd)) /bin/bash
