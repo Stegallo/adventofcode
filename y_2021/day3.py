90fe42b751690c52d67e12c0d50ccc311a901591
Stefano Gallotti <sgallotti@expediagroup.com>
2021-12-05 17:54:03 -0800

    day3 clean

--- y_2021/day3.py
+++ y_2021/day3.py
@@ -1,5 +1,110 @@
 from .common import AoCDay
 import copy
+from typing import Dict
+from .utils import decimal_from_binary
+
+
+class Element:
+    def __init__(self, digit):
+        self.__digit = int(digit)
+        self.__valid = True
+
+    def digit(self):
+        return self.__digit
+
+    def invalidate(self):
+        self.__valid = False
+
+    def validate(self):
+        self.__valid = True
+
+    def valid(self):
+        return self.__valid
+
+
+class Diagnostic:
+    def __init__(self):
+        self.__rows = []
+        self.__columns = []
+
+    def __repr__(self):
+        return "\n".join([str([row for row in rows]) for rows in self.__rows])
+
+    def reset(self):
+        for row in self.__rows:
+            for element in row:
+                element.validate()
+
+    def add_row(self, number):
+        self.__rows.append([])
+        for column, digit in enumerate(number):
+            element = Element(digit)
+            self.__rows[-1].append(element)
+            if column >= len(self.__columns):
+                self.__columns.append([])
+            self.__columns[column].append(element)
+
+    def gamma(self):
+        result = []
+        for x in self.__columns:
+            if sum([int(i.digit()) for i in x]) > len(x) // 2:
+                result.append(1)
+            else:
+                result.append(0)
+        return result
+
+    def epsilon(self):
+        result = []
+        for x in self.__columns:
+            if sum([int(i.digit()) for i in x]) < len(x) // 2:
+                result.append(1)
+            else:
+                result.append(0)
+        return result
+
+    def oxygen(self):
+        self.reset()
+        for c, x in enumerate(self.__columns):
+            keep = (
+                1
+                if sum([int(i.digit()) for i in x if i.valid()])
+                >= len([i for i in x if i.valid()]) / 2
+                else 0
+            )
+            for i in self.__rows:
+                if i[c].digit() != keep:
+                    for j in i:
+                        j.invalidate()
+
+            rows = []
+            for c, i in enumerate(self.__rows):
+                if temp := [x.digit() for x in i if x.valid()]:
+                    rows.append(temp)
+            if len(rows) == 1:
+                break
+        return rows[0]
+
+    def c02(self):
+        self.reset()
+        for c, x in enumerate(self.__columns):
+            keep = (
+                1
+                if sum([int(i.digit()) for i in x if i.valid()])
+                < len([i for i in x if i.valid()]) / 2
+                else 0
+            )
+            for i in self.__rows:
+                if i[c].digit() != keep:
+                    for j in i:
+                        j.invalidate()
+
+            rows = []
+            for c, i in enumerate(self.__rows):
+                if temp := [x.digit() for x in i if x.valid()]:
+                    rows.append(temp)
+            if len(rows) == 1:
+                break
+        return rows[0]
 
 
 class Day(AoCDay):
@@ -7,123 +112,16 @@ class Day(AoCDay):
         super().__init__(__name__.split(".")[1].replace("day", ""), test)
 
     def _preprocess_input(self):
-        self.__input_data = [i for i in self._input_data]
-        print(f"len of input = {len(self.__input_data)}")
-        try:
-            print(f"first value = {self.__input_data[0]}")
-            print(f"mid value = {self.__input_data[len(self.__input_data)//2]}")
-            print(f"last value = {self.__input_data[-1]}")
-        except:
-            ...
-
-    def __pivot(self, x):
-        p = []
-        for c, v in enumerate(x):
-            for d, u in enumerate(v):
-                if len(p) <= d:
-                    p.append([])
-                p[d].append(u)
-        # print(p)
-        return p
+        self.__diagnostic = Diagnostic()
+        for number in self._input_data:
+            self.__diagnostic.add_row(number)
 
     def _calculate_1(self):
-        x = self.__input_data
-        # p = []
-        # for c, v in enumerate(x):
-        #     for d, u in enumerate(v):
-        #         if len(p) <= d:
-        #             p.append([])
-        #         p[d].append(u)
-        # # print(p)
-        # p = self.pivot()
-        # r = []
-        # for i in p:
-        #     print(len(i))
-        #     if sum([int(j) for j in i]) > 500:
-        #         print(1)
-        #         r.append(1)
-        #     else:
-        #         print(0)
-        #         r.append(0)
-        # print(r)
-        # print([1 - i for i in r])
-        result = 0
-
-        #
-        # [0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1]
-        # [1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0]
-        #
-        # 001100100101
-        #
-        # 805
-        #
-        # 110011011010
-        #
-        # 3290
-        # 2648450
-        return result
-
-    def __inner_ox(self, x, p, incr):
-        result = []
-        # print(p)
-        for c, i in enumerate(p):
-            v = "1"
-            print(f"{len(i)=}")
-            # print(sum([int(j) for j in i]))
-            # print(sum([1 - int(j) for j in i]))
-            if sum([int(j) for j in i]) < sum([1 - int(j) for j in i]):
-                v = "0"
-            if c >= incr:
-                break
-            # print(f"{v=}")
-
-        for i in x:
-            if i[incr] == v:
-                result.append(i)
-        return result
-
-    def __inner_sc(self, x, p, incr):
-        result = []
-        for c, i in enumerate(p):
-            v = "0"
-            print(f"{len(i)=}")
-            # print(sum([int(j) for j in i]))
-            # print(sum([1 - int(j) for j in i]))
-            if sum([int(j) for j in i]) < sum([1 - int(j) for j in i]):
-                v = "1"
-            if c >= incr:
-                break
-
-        for i in x:
-            if i[incr] == v:
-                result.append(i)
-        return result
+        return decimal_from_binary(self.__diagnostic.gamma()) * decimal_from_binary(
+            self.__diagnostic.epsilon()
+        )
 
     def _calculate_2(self):
-        x = self.__input_data
-        y = self.__input_data
-        # p = self.__pivot(x)
-        # print(*p, sep="\n")
-        incr = 0
-        while len(x) > 1:
-            print(f"{len(x)=}")
-            x = self.__inner_ox(x, self.__pivot(x), incr)
-
-            incr += 1
-            print(f"{incr=}")
-        print(x)
-
-        incr = 0
-        while len(y) > 1:
-            print(f"{len(y)=}")
-            y = self.__inner_sc(y, self.__pivot(y), incr)
-            incr += 1
-            print(f"{incr=}")
-        print(y)
-        return 841 * 3384
-
-        # 001101001001
-        # 841
-
-        # 110100111000
-        # 3384
+        return decimal_from_binary(self.__diagnostic.oxygen()) * decimal_from_binary(
+            self.__diagnostic.c02()
+        )
