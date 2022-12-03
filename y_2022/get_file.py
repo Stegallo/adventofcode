abf8e4161f571d2c761b4576f91f76106e9a5979
Stegallo <stegallo@gmail.com>
2022-12-02 20:18:31 -0800

    get file

--- y_2022/get_file.py
+++ y_2022/get_file.py
@@ -1,11 +1,13 @@
 import requests
 
+from .secret import SESSION
+
 S = requests.Session()
 S.headers["User-Agent"] = "github.com/stegallo"
 URL = "https://adventofcode.com/{:d}/day/{:d}/{:s}"
 YEAR, DAY = 2022, 2
-S.cookies.set("session", "")
+S.cookies.set("session", SESSION)
 r = S.get(URL.format(YEAR, DAY, "input"))
 r.text
-with open("__test_input.txt", "w") as f:
+with open(f"y_{YEAR}/input_day{DAY}.txt", "w") as f:
     f.write(r.text)
