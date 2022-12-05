10db605cc58f5212812ef93b5b040456581a4714
Stefano Gallotti <stgallotti@egencia.com>
2022-12-05 06:15:39 -0800

    day5 refactor

--- y_2022/day5.py
+++ y_2022/day5.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from typing import Dict, List
+
 from .common import AoCDay
 
 
@@ -6,89 +9,49 @@ class Day(AoCDay):
         super().__init__(__name__.split(".")[1].replace("day", ""), test)
 
     def _preprocess_input(self):
-        # self.__input_data = [[int(i) for i in chunk] for chunk in self._input_data]
-        self.__input_data = self._input_data
+        self.__parse_stack()
+
+    def __parse_stack(self) -> None:
+        start = self._input_data[0]
+        self.__stack: Dict[int, List[str]] = {}
+        self.__max_rack = 0
+        for i in start[-1].strip().split("   "):
+            if not i:
+                break
+            self.__stack[int(i)] = []
+            self.__max_rack += 1
+        for i in range(len(start) - 2, -1, -1):
+            for j in range(self.__max_rack):
+                if container := start[i][j * 4 : (j + 1) * 4].strip():
+                    self.__stack[j + 1].append(container)
+
+    def __produce_output(self, stack) -> str:
+        r = [
+            stack[i + 1].pop().replace("[", "").replace("]", "")
+            for i in range(self.__max_rack)
+        ]
+
+        return "".join(r)
 
     def _calculate_1(self):
-        start = self.__input_data[0]
-        moves = self.__input_data[1]
-        print(start[-1].split("   "))
-        d = {}
-        max_rack = 0
-        for i in start[-1][1:].split("   "):
-            d[int(i)] = []
-            max_rack += 1
-        for i in range(len(start) - 2, -1, -1):
-            # print(i)
-            # print(start[i])
-            for j in range(max_rack):
-                # print(d[str(j+1)])
-                container = start[i][j * 4 : (j + 1) * 4].strip()
-                if container:
-                    d[j + 1].append(container)
-                # print(start[i][j*4:(j+1)*4])
-        print(f"{d=}")
+        moves = self._input_data[1]
+        local_stack = deepcopy(self.__stack)
         for i in moves:
-            # print(i.split(' '))
-            (_, quantity, _, fro, _, to) = i.split(" ")
-            print(quantity, fro, to)
-            for j in range(int(quantity)):
-                # print(j)
-                d[int(to)].append(d[int(fro)].pop())
-        print(f"{d=}")
-        r = []
-        for i in range(max_rack):
-            r.append(d[i + 1].pop().replace("[", "").replace("]", ""))
-            # break
-        print("".join(r))
-        # print(f"{d=}")
-        # start[:-1]:
-        # print(i)
+            (_, quantity, _, source, _, destination) = i.split(" ")
 
-        # print(max_rack)
-        # print(moves)
-        return 0
+            for _ in range(int(quantity)):
+                local_stack[int(destination)].append(local_stack[int(source)].pop())
+        return self.__produce_output(local_stack)
 
     def _calculate_2(self):
-        start = self.__input_data[0]
-        moves = self.__input_data[1]
-        print(start[-1].split("   "))
-        d = {}
-        max_rack = 0
-        for i in start[-1][1:].split("   "):
-            d[int(i)] = []
-            max_rack += 1
-        for i in range(len(start) - 2, -1, -1):
-            # print(i)
-            # print(start[i])
-            for j in range(max_rack):
-                # print(d[str(j+1)])
-                container = start[i][j * 4 : (j + 1) * 4].strip()
-                if container:
-                    d[j + 1].append(container)
-                # print(start[i][j*4:(j+1)*4])
-        print(f"{d=}")
+        moves = self._input_data[1]
+        local_stack = deepcopy(self.__stack)
         for i in moves:
-            # print(i.split(' '))
-            (_, quantity, _, fro, _, to) = i.split(" ")
-            print(quantity, fro, to)
-            t = []
-            for j in range(int(quantity)):
-                # print(j)
-                t.insert(0, d[int(fro)].pop())
-            d[int(to)].extend(t)
-            # break
-        print(f"{d=}")
-        # return
-        r = []
-        for i in range(max_rack):
-            r.append(d[i + 1].pop().replace("[", "").replace("]", ""))
-            # break
-        print("".join(r))
-        # print(f"{d=}")
-        # start[:-1]:
-        # print(i)
+            (_, quantity, _, source, _, destination) = i.split(" ")
 
-        # print(max_rack)
-        # print(moves)
-        return 0
+            t = []
+            for _ in range(int(quantity)):
+                t.insert(0, local_stack[int(source)].pop())
+            local_stack[int(destination)].extend(t)
+
+        return self.__produce_output(local_stack)
