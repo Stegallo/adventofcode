12016e66faec37829684227f1de23a58f86e7a01
Stefano Gallotti <stgallotti@egencia.com>
2022-12-08 19:50:23 -0800

    test cases

--- tests/y_2022/test_2022_day8.py
+++ tests/y_2022/test_2022_day8.py
@@ -5,14 +5,14 @@ from unittest.mock import mock_open, patch
 from y_2022.common import load_input
 from y_2022.day8 import Day
 
-with patch("builtins.open", mock_open(read_data="0")):
+with patch("builtins.open", mock_open(read_data="01\n23")):
     day = Day()
 
 
 def test__preprocess_input():
-    day._input_data = [[]]
-    # day._preprocess_input()
-    # assert day._Day__input_data == []
+    day._input_data = [["01", "23"]]
+    day._preprocess_input()
+    assert day._Day__grid == {(0, 0): "0", (0, 1): "1", (1, 0): "2", (1, 1): "3"}
 
 
 def test_complete():
