e601b6902f8b805b1f69e3c1ff859eb0a16840b2
Stegallo <stegallo@gmail.com>
2023-11-03 21:15:56 -0700

    day2

--- tests/y_2015/test_2015_day2.py
+++ tests/y_2015/test_2015_day2.py
@@ -9,20 +9,20 @@ with patch("builtins.open", mock_open(read_data="1x1x1")):
 
 
 def test_calculate_1():
-    day._input_data= [["2x3x4"]]
+    day._input_data = [["2x3x4"]]
     day._preprocess_input()
     assert day._calculate_1() == 58
 
-    day._input_data= [["1x1x10"]]
+    day._input_data = [["1x1x10"]]
     day._preprocess_input()
     assert day._calculate_1() == 43
 
 
-def test_calculate_1():
-    day._input_data= [["2x3x4"]]
+def test_calculate_2():
+    day._input_data = [["2x3x4"]]
     day._preprocess_input()
     assert day._calculate_2() == 34
 
-    day._input_data= [["1x1x10"]]
+    day._input_data = [["1x1x10"]]
     day._preprocess_input()
     assert day._calculate_2() == 14
--- y_2015/day2.py
+++ y_2015/day2.py
@@ -10,18 +10,18 @@ class Present:
 
     @property
     def wrapping_paper(self):
-        side1 = self.length*self.width
-        side2 = self.width*self.height
-        side3 = self.height*self.length
+        side1 = self.length * self.width
+        side2 = self.width * self.height
+        side3 = self.height * self.length
 
-        return 2*side1 + 2*side2 + 2*side3 + min(side1, side2, side3)
+        return 2 * side1 + 2 * side2 + 2 * side3 + min(side1, side2, side3)
 
     @property
     def ribbon(self):
-        ordered_measures = sorted([self.length,self.width,self.height])
-        bow = self.length*self.width*self.height
+        ordered_measures = sorted([self.length, self.width, self.height])
+        bow = self.length * self.width * self.height
 
-        return 2*ordered_measures[0] + 2*ordered_measures[1] + bow
+        return 2 * ordered_measures[0] + 2 * ordered_measures[1] + bow
 
 
 class Day(AoCDay):
@@ -29,7 +29,7 @@ class Day(AoCDay):
         super().__init__(__name__, test)
 
     def _preprocess_input(self):
-        self.__input_data = [Present(*i.split('x')) for i in self._input_data[0]]
+        self.__input_data = [Present(*i.split("x")) for i in self._input_data[0]]
 
     def _calculate_1(self):
         return sum(present.wrapping_paper for present in self.__input_data)
