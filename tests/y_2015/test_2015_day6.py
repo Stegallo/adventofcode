db4279ac2a95373edf87e00db74d72b20c6517eb
Stegallo <stegallo@gmail.com>
2023-11-06 20:29:39 -0800

    day6

--- tests/y_2015/test_2015_day6.py
+++ tests/y_2015/test_2015_day6.py
@@ -9,12 +9,28 @@ with patch("builtins.open", mock_open(read_data="command 0,0 through 999,999")):
 
 
 def test_calculate_1():
-    day._input_data = [["turn on 0,0 through 999,0"]]
+    day._input_data = [["turn on 0,0 through 9,0"]]
     day._preprocess_input()
-    assert day._calculate_1() == 1000
+    assert day._calculate_1() == 10
+
+    day._input_data = [["turn off 0,0 through 9,0"]]
+    day._preprocess_input()
+    assert day._calculate_1() == 0
+
+    day._input_data = [["toggle 0,0 through 9,0"]]
+    day._preprocess_input()
+    assert day._calculate_1() == 10
 
 
 def test_calculate_2():
     day._input_data = [["turn on 0,0 through 0,0"]]
     day._preprocess_input()
     assert day._calculate_2() == 1
+
+    day._input_data = [["turn off 0,0 through 0,0"]]
+    day._preprocess_input()
+    assert day._calculate_2() == 0
+
+    day._input_data = [["toggle 0,0 through 0,0"]]
+    day._preprocess_input()
+    assert day._calculate_2() == 2
