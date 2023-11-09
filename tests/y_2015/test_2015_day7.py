a15b30bf31c0e6c4a316b709e048ea741a8a5374
Stegallo <stegallo@gmail.com>
2023-11-09 14:54:41 -0800

    day 7

--- tests/y_2015/test_2015_day7.py
+++ tests/y_2015/test_2015_day7.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from unittest.mock import mock_open, patch
 
-from y_2015.day7 import Day
+from y_2015.day7 import Day, Ingress
 
 with patch(
     "builtins.open",
@@ -22,8 +22,34 @@ with patch(
 
 
 def test_calculate_1():
-    assert day._calculate_1() == "123"
+    assert day._calculate_1() == 123
 
 
 def test_calculate_2():
-    assert day._calculate_2() == "123"
+    assert day._calculate_2() == 123
+
+
+def test_ingress_resolve():
+    ingress = Ingress("123")
+    assert ingress.resolve({}) == Ingress("123")
+
+    ingress = Ingress("a")
+    assert ingress.resolve({"a": Ingress("1")}) == Ingress("1")
+
+    ingress = Ingress("NOT 1")
+    assert ingress.resolve({}) == Ingress("65534")
+
+    ingress = Ingress("NOT a")
+    assert ingress.resolve({"a": Ingress("1")}) == Ingress("65534")
+
+    ingress = Ingress("x AND y")
+    assert ingress.resolve({"x": Ingress("123"), "y": Ingress("456")}) == Ingress("72")
+
+    ingress = Ingress("x OR y")
+    assert ingress.resolve({"x": Ingress("123"), "y": Ingress("456")}) == Ingress("507")
+
+    ingress = Ingress("x LSHIFT 2")
+    assert ingress.resolve({"x": Ingress("123")}) == Ingress("492")
+
+    ingress = Ingress("y RSHIFT 2")
+    assert ingress.resolve({"y": Ingress("456")}) == Ingress("114")
--- y_2015/day7.py
+++ y_2015/day7.py
@@ -1,9 +1,34 @@
 import re
-from typing import Dict, Optional, Self
+from pprint import pprint
+from typing import Dict, List, Optional, Self
 
 from pydantic.dataclasses import dataclass
 
 from common.aoc import AoCDay
+
+
+def bitand(x, y):
+    return x & y
+
+
+def bitor(x, y):
+    return x | y
+
+
+def lshift(x, y):
+    return x << y
+
+
+def rshift(x, y):
+    return x >> y
+
+
+def bitnot(x):
+    return x ^ 65535
+
+
+BIN_OPERATORS = {"AND": bitand, "OR": bitor, "LSHIFT": lshift, "RSHIFT": rshift}
+UN_OPERATORS = {"NOT": bitnot}
 
 
 @dataclass
@@ -89,10 +114,78 @@ class Command:
         return self.result
 
 
+class Ingress:
+    @staticmethod
+    def extract_opcode(source: str) -> Optional[str]:
+        r = re.search(r"(NOT|AND|OR|LSHIFT|RSHIFT)", source)
+        if r:
+            return r.groups()[0]
+        return None
+
+    @staticmethod
+    def extract_inputs(source: str, opcode: Optional[str]) -> List[str]:
+        if not opcode:
+            return [source]
+        if opcode == "NOT":
+            return [source.replace("NOT ", "")]
+        return source.replace(opcode, "").split("  ")
+
+    def __init__(self, source) -> None:
+        self.opcode: Optional[str] = self.extract_opcode(source)
+        self.inputs: List[str] = self.extract_inputs(source, self.opcode)
+        self.result: Optional[int] = None
+        if self.inputs[0].isnumeric():
+            self.result = int(self.inputs[0])
+
+    def __repr__(self):
+        return (
+            f"Ingress(opcode={self.opcode}, inputs={self.inputs}, result={self.result})"
+        )
+
+    def __eq__(self, other):
+        if isinstance(other, Ingress):
+            return self.opcode == other.opcode and self.inputs == other.inputs
+        return False
+
+    @property
+    def value(self):
+        return self.result
+
+    def resolve(self, wires):
+        # print(self)
+        if not self.opcode:
+            if self.inputs[0].isnumeric():
+                self.result = int(self.inputs[0])
+                return self
+            return wires[self.inputs[0]].resolve(wires)
+        if self.opcode in UN_OPERATORS:
+            if self.inputs[0].isnumeric():
+                op1 = int(self.inputs[0])
+            else:
+                op1 = wires[self.inputs[0]].resolve(wires).value
+            return Ingress(str(UN_OPERATORS[self.opcode](op1)))
+        if self.opcode in BIN_OPERATORS:
+            if self.inputs[0].isnumeric():
+                op1 = int(self.inputs[0])
+            else:
+                op1 = wires[self.inputs[0]].resolve(wires).value
+            if self.inputs[1].isnumeric():
+                op2 = int(self.inputs[1])
+            else:
+                op2 = wires[self.inputs[1]].resolve(wires).value
+            return Ingress(str(BIN_OPERATORS[self.opcode](op1, op2)))
+        return 1 / 0
+
+
 @dataclass
 class Instruction:
-    command_str: str
-    destination: str
+    source: str
+    target: str
+    ingress: Optional[Ingress] = None
+
+    def __post_init__(self):
+        # transforms source in an Ingress object
+        self.ingress = Ingress(self.source)
 
     @property
     def command(self):
@@ -114,16 +207,23 @@ class Day(AoCDay):
     def _calculate_1(self):
         wires = {}
         for x in self.__input_data:
-            wires[x.destination] = x.command
+            wires[x.target] = x.ingress
 
-        return wires["a"].operate(wires)
+        pprint(wires)
+        # for x in self.__input_data:
+        #     print(x.target)
+        #     wires[x.target] = wires[x.target].resolve(wires)
+        # pprint(wires)
+        wires["a"].resolve(wires).value
+        return 0  # wires["a"].resolve(wires).value
 
     def _calculate_2(self):
+        return 0
         wires = {}
         for x in self.__input_data:
-            if x.destination == "b":
-                wires[x.destination] = Command("46065")
+            if x.target == "b":
+                wires[x.target] = Ingress("46065")
             else:
-                wires[x.destination] = x.command
-
-        return wires["a"].operate(wires)
+                wires[x.target] = x.ingress
+        #
+        return wires["a"].resolve(wires).value
