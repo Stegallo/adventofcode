e0c8a6dc76f58151a6c896100b8c7175b935e87d
Stegallo <stegallo@gmail.com>
2023-11-10 16:17:31 -0800

    day7

--- y_2015/day7.py
+++ y_2015/day7.py
@@ -1,109 +1,85 @@
-import re
-
-# from typing import Dict, Optional, Self, List
-from typing import Optional, List
-
 from pydantic.dataclasses import dataclass
-
 from common.aoc import AoCDay
+from typing import Optional, List, Dict, Any
 
 
-def bitand(x, y):
+def bitand(x, y) -> int:
     return x & y
 
 
-def bitor(x, y):
+def bitor(x, y) -> int:
     return x | y
 
 
-def lshift(x, y):
+def lshift(x, y) -> int:
     return x << y
 
 
-def rshift(x, y):
+def rshift(x, y) -> int:
     return x >> y
 
 
-def bitnot(x):
+def bitnot(x) -> int:
     return x ^ 65535
 
 
 BIN_OPERATORS = {"AND": bitand, "OR": bitor, "LSHIFT": lshift, "RSHIFT": rshift}
 UN_OPERATORS = {"NOT": bitnot}
+OPERATORS: Dict[str, Any] = {
+    "NOT": bitnot,
+    "AND": bitand,
+    "OR": bitor,
+    "LSHIFT": lshift,
+    "RSHIFT": rshift,
+}
 
 
-class Ingress:
-    @staticmethod
-    def extract_opcode(source: str) -> Optional[str]:
-        r = re.search(r"(NOT|AND|OR|LSHIFT|RSHIFT)", source)
-        if r:
-            return r.groups()[0]
-        return None
+@dataclass
+class Element:
+    source: str
+    is_operator: bool = False
+    operator: Optional[str] = None
+    inputs: Optional[List[str]] = None
+    result: int = 0
+    result_valid: bool = False
 
-    @staticmethod
-    def extract_inputs(source: str, opcode: Optional[str]) -> List[str]:
-        if not opcode:
-            return [source]
-        if opcode == "NOT":
-            return [source.replace("NOT ", "")]
-        return source.replace(opcode, "").split("  ")
+    def __post_init__(self) -> None:
+        self.is_operator = any([o in self.source for o in list(OPERATORS.keys())])
+        for i in list(OPERATORS.keys()):
+            if i in self.source:
+                self.operator = i
+        if self.is_operator:
+            self.inputs = [
+                x for x in self.source.replace(" ", "").split(self.operator) if x
+            ]
 
-    def __init__(self, source) -> None:
-        self.opcode: Optional[str] = self.extract_opcode(source)
-        self.inputs: List[str] = self.extract_inputs(source, self.opcode)
-        self.result: Optional[int] = None
-        if self.inputs[0].isnumeric():
-            self.result = int(self.inputs[0])
+    def resolve(self, wires) -> int:
+        if self.result_valid:
+            return self.result
+        if not self.is_operator:
+            if self.source.isnumeric():
+                self.result = int(self.source)
+                self.result_valid = True
+            else:
+                self.result = wires[self.source].resolve(wires)
+                self.result_valid = True
 
-    def __repr__(self):
-        return (
-            f"Ingress(opcode={self.opcode}, inputs={self.inputs}, result={self.result})"
-        )
+        else:
+            inputs = [x for x in self.source.replace(" ", "").split(self.operator) if x]
 
-    def __eq__(self, other):
-        if isinstance(other, Ingress):
-            return self.opcode == other.opcode and self.inputs == other.inputs
-        return False
+            inputs = [
+                int(i) if i.isnumeric() else wires[i].resolve(wires) for i in inputs
+            ]
 
-    @property
-    def value(self):
+            self.result = OPERATORS[str(self.operator)](*inputs)
+            self.result_valid = True
         return self.result
-
-    def resolve(self, wires):
-        # print(self)
-        if not self.opcode:
-            if self.inputs[0].isnumeric():
-                self.result = int(self.inputs[0])
-                return self
-            return wires[self.inputs[0]].resolve(wires)
-        if self.opcode in UN_OPERATORS:
-            if self.inputs[0].isnumeric():
-                op1 = int(self.inputs[0])
-            else:
-                op1 = wires[self.inputs[0]].resolve(wires).value
-            return Ingress(str(UN_OPERATORS[self.opcode](op1)))
-        if self.opcode in BIN_OPERATORS:
-            if self.inputs[0].isnumeric():
-                op1 = int(self.inputs[0])
-            else:
-                op1 = wires[self.inputs[0]].resolve(wires).value
-            if self.inputs[1].isnumeric():
-                op2 = int(self.inputs[1])
-            else:
-                op2 = wires[self.inputs[1]].resolve(wires).value
-            return Ingress(str(BIN_OPERATORS[self.opcode](op1, op2)))
-        return 1 / 0
 
 
 @dataclass
 class Instruction:
     source: str
     target: str
-    ingress: Optional[str] = None
-
-    def __post_init__(self):
-        # transforms source in an Ingress object
-        self.ingress = Ingress(self.source)
 
 
 class Day(AoCDay):
@@ -112,32 +88,22 @@ class Day(AoCDay):
 
     def _preprocess_input(self):
         self.__input_data = [
-            Instruction(
-                *list(re.search(r"(.*) -> (\D*)", i).groups()),
-            )
-            for i in self._input_data[0]
+            Instruction(*list(i.split(" -> "))) for i in self._input_data[0]
         ]
 
     def _calculate_1(self):
         wires = {}
         for x in self.__input_data:
-            wires[x.target] = x.ingress
+            wires[x.target] = Element(x.source)
 
-        # pprint(wires)
-        # for x in self.__input_data:
-        #     print(x.target)
-        #     wires[x.target] = wires[x.target].resolve(wires)
-        # pprint(wires)
-        wires["a"].resolve(wires).value
-        return 0  # wires["a"].resolve(wires).value
+        return wires["a"].resolve(wires)
 
     def _calculate_2(self):
-        return 0
         wires = {}
         for x in self.__input_data:
             if x.target == "b":
-                wires[x.target] = Ingress("46065")
+                wires[x.target] = Element("46065")
             else:
-                wires[x.target] = x.ingress
-        #
-        return wires["a"].resolve(wires).value
+                wires[x.target] = Element(x.source)
+
+        return wires["a"].resolve(wires)
