8ad6cbe261bcfb07b5020000748303b5bd49aa91
Stegallo <stegallo@gmail.com>
2023-11-11 22:13:30 +0000

    day9

---
+++ y_2015/day9.py
@@ -0,0 +1,21 @@
+from common.aoc import AoCDay
+
+
+class Day(AoCDay):
+    def __init__(self, test=0):
+        super().__init__(__name__, test)
+
+    def _preprocess_input(self):
+        # self.__input_data = [[int(i) for i in chunk] for chunk in self._input_data]
+        print(f"{self._input_data=}")
+        self.__input_data = self._input_data[0]
+
+    def _calculate_1(self):
+        x = self.__input_data
+        print(f"{x=}")
+        return 0
+
+    def _calculate_2(self):
+        x = self.__input_data
+        print(f"{x=}")
+        return 0
