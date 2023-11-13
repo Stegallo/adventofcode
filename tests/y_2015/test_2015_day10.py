81048999d2e75f453f142c33f14170a78bfb613d
Stegallo <stegallo@gmail.com>
2023-11-13 15:22:23 -0800

    day10

--- tests/y_2015/test_2015_day10.py
+++ tests/y_2015/test_2015_day10.py
@@ -8,9 +8,31 @@ with patch("builtins.open", mock_open(read_data="")):
     day = Day()
 
 
+def test_apply_times():
+    assert day._apply_times("1", 1) == "11"
+    assert day._apply_times("11", 1) == "21"
+
+    assert day._apply_times("1", 2) == "21"
+    assert day._apply_times("1", 3) == "1211"
+    assert day._apply_times("1", 4) == "111221"
+    assert day._apply_times("1", 5) == "312211"
+
+
 def test_calculate_1():
     day._Day__input_data = ["1"]
     assert day._calculate_1() == "11"
+
+    day._Day__input_data = ["1", "1"]
+    assert day._calculate_1() == "21"
+
+    day._Day__input_data = ["2", "1"]
+    assert day._calculate_1() == "1211"
+
+    day._Day__input_data = ["1", "2", "1", "1"]
+    assert day._calculate_1() == "111221"
+
+    day._Day__input_data = ["1", "1", "1", "2", "2", "1"]
+    assert day._calculate_1() == "312211"
 
 
 def test_calculate_2():
--- y_2015/day10.py
+++ y_2015/day10.py
@@ -6,16 +6,36 @@ class Day(AoCDay):
         super().__init__(__name__, test)
 
     def _preprocess_input(self):
-        # self.__input_data = [[int(i) for i in chunk] for chunk in self._input_data]
-        print(f"{self._input_data=}")
-        self.__input_data = self._input_data[0]
+        self.__input_data = self._input_data[0][0]
+
+    def say_loud(self, x: str) -> str:
+        if len(x) == 1:
+            return "1" + x[0]
+        chunks = []
+        chunk = [x[0]]
+        for c in range(len(x)):
+            if c == 0:
+                continue
+
+            if x[c - 1] == x[c]:
+                chunk.append(x[c])
+            else:
+                chunks.append(chunk)
+                chunk = [x[c]]
+        chunks.append(chunk)
+        k = []
+        for i in chunks:
+            k.append(str(len(i)) + i[0])
+        return "".join(k)
+
+    def _apply_times(self, input: str, times: int) -> str:
+        result = input
+        for _ in range(times):
+            result = self.say_loud(result)
+        return result
 
     def _calculate_1(self):
-        x = self.__input_data
-        print(f"{x=}")
-        return 0
+        return len(self._apply_times(self.__input_data, 40))
 
     def _calculate_2(self):
-        x = self.__input_data
-        print(f"{x=}")
-        return 0
+        return len(self._apply_times(self.__input_data, 50))
