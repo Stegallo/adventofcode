3e77a5a4b14bd3fa1e6db95b079bdfbfe333f22d
Stegallo <stegallo@gmail.com>
2023-12-15 14:59:44 -0800

    test day7

--- tests/y_2023/test_2023_day7.py
+++ tests/y_2023/test_2023_day7.py
@@ -4,7 +4,7 @@ from unittest.mock import mock_open, patch
 
 from y_2023.day7 import Day, Hand
 
-with patch("builtins.open", mock_open(read_data="0")):
+with patch("builtins.open", mock_open(read_data="0 1")):
     day = Day()
 
 
@@ -13,43 +13,43 @@ def test_Hand():
     h = Hand("32T3K")
     h = Hand("3333K")
     print(h)
-    print(h.rate())
+    print(h.rate("original"))
     assert True
 
 
 def test_Hand_five():
     h = Hand("AAAAA")
-    assert h.rate() == ("Five of a kind", "A")
+    assert h.rate("original") == ("Five of a kind")  # , "A")
 
 
 def test_Hand_four():
     h = Hand("AA8AA")
-    assert h.rate() == ("Four of a kind", "A")
+    assert h.rate("original") == ("Four of a kind")  # , "A")
 
 
 def test_Hand_full():
     h = Hand("23332")
-    assert h.rate() == ("Full house", "3", "2")
+    assert h.rate("original") == ("Full house")  # , "3", "2")
 
 
 def test_Hand_three():
     h = Hand("AA8A9")
-    assert h.rate() == ("Three of a kind", "A")
+    assert h.rate("original") == ("Three of a kind")  # , "A")
 
 
 def test_Hand_two_pair():
     h = Hand("AA898")
-    assert h.rate() == ("Two pair", "A", "8")
+    assert h.rate("original") == ("Two pair")  # , "A", "8")
 
 
 def test_Hand_one_pair():
     h = Hand("AA8J9")
-    assert h.rate() == ("One pair", "A")
+    assert h.rate("original") == ("One pair")  # , "A")
 
 
 def test_Hand_high_card():
     h = Hand("23456")
-    assert h.rate() == ("High card", None)
+    assert h.rate("original") == ("High card")  # , None)
 
 
 def test__preprocess_input():
