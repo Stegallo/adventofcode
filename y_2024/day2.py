61fb6f3108c1005665627ea25ae04c6398058e35
Stegallo <stegallo@gmail.com>
2024-12-02 20:44:29 -0800

    day2 cleanup

--- y_2024/day2.py
+++ y_2024/day2.py
@@ -8,10 +8,38 @@ from common.aoc import AoCDay
 @dataclass
 class Row:
     original: str
-    processed: Optional[str] = None
+    report: Optional[list[int]] = None
 
     def __post_init__(self) -> None:
-        self.processed = [int(i) for i in self.original.split()]
+        self.report = [int(i) for i in self.original.split()]
+
+    @property
+    def safe(self) -> bool:
+        return self.is_report_safe(self.report)  # type: ignore
+
+    @property
+    def dampener_safe(self) -> bool:
+        if self.safe:
+            return True
+        for remove in range(len(self.report)):  # type: ignore
+            dampened_report = (
+                self.report[:remove]  # type: ignore
+                + self.report[remove + 1 :]  # type: ignore
+            )
+            if self.is_report_safe(dampened_report):
+                return True
+        return False
+
+    @staticmethod
+    def is_report_safe(report: list[int]) -> bool:
+        all_increasing = True
+        all_decreasing = True
+        for i in range(len(report) - 1):
+            if report[i + 1] - report[i] not in (1, 2, 3):
+                all_increasing = False
+            if report[i] - report[i + 1] not in (1, 2, 3):
+                all_decreasing = False
+        return all_increasing or all_decreasing
 
 
 class Day(AoCDay):
@@ -19,58 +47,10 @@ class Day(AoCDay):
         super().__init__(__name__, test)
 
     def _preprocess_input(self):
-        # self.__input_data = [[int(i) for i in chunk] for chunk in self._input_data]
-        print(f"{self._input_data=}")
         self.__input_data = [Row(i) for i in self._input_data[0]]
 
     def _calculate_1(self):
-        r = 0
-        for x in self.__input_data:
-            y = x.processed
-            si = True
-            sd = True
-            for c, i in enumerate(y):
-                if c == 0:
-                    continue
-                if y[c] - y[c - 1] not in (1, 2, 3):
-                    si = False
-                if y[c - 1] - y[c] not in (1, 2, 3):
-                    sd = False
-            if si or sd:
-                r += 1
-        return r
+        return sum(x.safe for x in self.__input_data)
 
     def _calculate_2(self):
-        r = 0
-        for x in self.__input_data:
-            y = x.processed
-            si = True
-            sd = True
-            for c, i in enumerate(y):
-                if c == 0:
-                    continue
-                if y[c] - y[c - 1] not in (1, 2, 3):
-                    si = False
-                if y[c - 1] - y[c] not in (1, 2, 3):
-                    sd = False
-            if si or sd:
-                r += 1
-            else:
-                found = False
-                for rem in range(len(y)):
-                    if found:
-                        break
-                    new_y = y[:rem] + y[rem + 1 :]
-                    ssi = True
-                    ssd = True
-                    for c, i in enumerate(new_y):
-                        if c == 0:
-                            continue
-                        if new_y[c] - new_y[c - 1] not in (1, 2, 3):
-                            ssi = False
-                        if new_y[c - 1] - new_y[c] not in (1, 2, 3):
-                            ssd = False
-                    if ssi or ssd:
-                        r += 1
-                        found = True
-        return r
+        return sum(x.dampener_safe for x in self.__input_data)
