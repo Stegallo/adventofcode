1e72dbf17f4e5c60e69c04fd55abb8c6e73c6b29
Stegallo <stegallo@gmail.com>
2024-12-03 18:38:49 -0800

    test cases day3

--- y_2024/day3.py
+++ y_2024/day3.py
@@ -1,5 +1,5 @@
-from typing import Optional
 import re
+
 from pydantic.dataclasses import dataclass
 
 from common.aoc import AoCDay
@@ -8,10 +8,7 @@ from common.aoc import AoCDay
 @dataclass
 class Row:
     original: str
-    # processed: Optional[str] = None
 
-    def __post_init__(self) -> None:
-        self.processed = ""  # self.original
 
 @dataclass
 class Mul:
@@ -20,11 +17,13 @@ class Mul:
 
     @staticmethod
     def from_str(string):
-        return (lambda x,y: Mul(int(x),int(y))) (*string.replace(')','').replace('mul(', '').split(','))
+        return (lambda x, y: Mul(int(x), int(y)))(
+            *string.replace(")", "").replace("mul(", "").split(",")
+        )
 
     @property
     def prod(self):
-        return self.op1*self.op2
+        return self.op1 * self.op2
 
 
 class Day(AoCDay):
@@ -36,10 +35,10 @@ class Day(AoCDay):
 
     def _calculate_1(self):
         result = 0
-        p = re.compile(r'mul\(\d{1,3}\,\d{1,3}\)')
+        p = re.compile(r"mul\(\d{1,3}\,\d{1,3}\)")
         for j in self.__input_data:
             for i in p.findall(j.original):
-                result+= Mul.from_str(i).prod
+                result += Mul.from_str(i).prod
         return result
 
     def _calculate_2(self):
@@ -48,11 +47,11 @@ class Day(AoCDay):
         do = True
         for j in self.__input_data:
             for i in p.findall(j.original):
-                if i == 'do()':
+                if i == "do()":
                     do = True
-                elif i == 'don\'t()':
+                elif i == "don't()":
                     do = False
                     continue
-                elif 'mul' in i and do:
-                    result+= Mul.from_str(i).prod
+                elif "mul" in i and do:
+                    result += Mul.from_str(i).prod
         return result
