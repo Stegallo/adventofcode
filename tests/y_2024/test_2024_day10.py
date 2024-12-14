4b2433961ea486cc9a836d3cd40415f75b3c988a
Stegallo <stegallo@gmail.com>
2024-12-14 15:29:09 -0800

    day 10

--- tests/y_2024/test_2024_day10.py
+++ tests/y_2024/test_2024_day10.py
@@ -7,18 +7,14 @@ from y_2024.day10 import Day
 with patch(
     "builtins.open",
     mock_open(
-        read_data="""............
-........0...
-.....0......
-.......0....
-....0.......
-......A.....
-............
-............
-........A...
-.........A..
-............
-............
+        read_data="""89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
 """,  # noqa: E501
     ),
 ):
@@ -31,9 +27,9 @@ def test__preprocess_input():
 
 def test_calculate_1():
     r = day._calculate_1()
-    assert r == 14
+    assert r == 36
 
 
 def test_calculate_2():
     r = day._calculate_2()
-    assert r == 34
+    assert r == 81
